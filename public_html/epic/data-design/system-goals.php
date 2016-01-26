eb1f3521a9cc1e7de3313a95b6769c53801e37b4
Elaine Thomas <enajera2@cnm.edu>
2016-01-26 15:55:19 -0700

    adding executive summary and system goals for data design

--- public_html/epic/data-design/system-goals.php
+++ public_html/epic/data-design/system-goals.php
@@ -7,12 +7,13 @@
 		</title>
 	</head>
 	<body>
+		<h3>Executive Summary</h3>
 		<p>
-			Executive Summary: Time Cruncher is a website dedicated to assisting Management and Employees in creating a dynamic schedule that can be accessed, and updated in real time.
+			Time Cruncher is a website dedicated to assisting Management and Employees in creating a dynamic schedule that can be accessed, and updated in real time.
     	</p>
-
+		<h3>System Goals</h3>
 		<p>
-			System Goals - There will be two main types of users, the employees team and management team.  A member of the employee team will use the website to view their current scheduled working hours, document days/times that they are available to work or block out days that they are not able to work or request time off.   Members of the management team will access the website to review schedules and set minimum standards for number/type of employees needed to staff a shift.
+			There will be two main types of users, the employees team and management team.  A member of the employee team will use the website to view their current scheduled working hours, document days/times that they are available to work or block out days that they are not able to work or request time off.   Members of the management team will access the website to review schedules and set minimum standards for number/type of employees needed to staff a shift.
 		</p>
 	</body>
 </html>
