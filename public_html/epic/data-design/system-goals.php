eaa1d760470bdf8a02e32566890d478656626c6e
Elaine Thomas <enajera2@cnm.edu>
2016-01-26 15:37:40 -0700

    adding executive summary and system goals for data design

--- public_html/epic/data-design/system-goals.php
+++ public_html/epic/data-design/system-goals.php
@@ -12,7 +12,7 @@
     	</p>
 
 		<p>
-			System Goals - There will be two main types of users, the employees team and management team.  A memeber of the employee team will use the website to view their current scheduled working hours, document days/times that they are available to work or block out days that they are not able to work or request time off.   Members of the management team will access the website to review scheudles and set minimum standards for number/type of employees needed to staff a shift.
+			System Goals - There will be two main types of users, the employees team and management team.  A member of the employee team will use the website to view their current scheduled working hours, document days/times that they are available to work or block out days that they are not able to work or request time off.   Members of the management team will access the website to review schedules and set minimum standards for number/type of employees needed to staff a shift.
 		</p>
 	</body>
 </html>
