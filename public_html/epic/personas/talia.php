9012b6d5c30b848422359646ea1673148e06817d
Elaine Thomas <enajera2@cnm.edu>
2016-01-27 09:32:11 -0700

    add new persona (in progress)

---
+++ public_html/epic/personas/talia.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+	<html>
+		<head>
+			<title>Persona - Talia</title>
+		</head>
+	<body>
+		<p>
+			Talia, 27, fast-food worker
+			Talia is a smartphone user and has an older laptop at home that she used to use for homework but now uses primarily for streaming video.  Generally, she uses her smartphone to access the Internet.  She likes her job well enough but she frequently has problems with the scheduling.  When she wants to make plans with her friends she has to drive to her workplace the Sunday before to find out when her manager has scheduled her.  Also,
+		</p>
+	</body>
+	</html>
