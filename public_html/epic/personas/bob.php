2a6d0762cd7acff4ba4a42d57e5f9e0992f3c097
Sam Chandler <samuelvanchandler@gmail.com>
2016-01-27 14:21:44 -0700

    test

--- public_html/epic/personas/bob.php
+++ public_html/epic/personas/bob.php
@@ -2,19 +2,15 @@
 <html lang="en">
 	<head>
 		<meta charset="utf-8"/>
-		<title>Sam's persona</title>
+		<title>Sam's persona's</title>
 	</head>
 	<body>
 		<h1>Persona</h1>
 		<h2>Bob</h2>
-		<p>Bob is the the owner of a restaurant, he has 18 employees. He finds he spends
+		<p>
+			Bob is the the owner of a restaurant, he has 18 employees. He finds he spends
 			a lot of excess time on scheduling his employees' work shifts, accommodating their
 			schedule requests and preferences, keeping track of their hours, etc.
 		</p>
-			Suzy is the manager of a large firm that sends small teams to several physical locations
-			daily to preform a service. Each team requires a specialist A and a specialist B, an
-			employee can ether be a specialist A, specialist B, or an apprentice. And there must be a
-			1 to 1 ratio fo specialists to apprentices.  Managing over 50 employees, and a schedule of
-			over 10 locations ever day, Suzy finds her current scheduling solution doesn't cut it.
 	</body>
 </html>
---
+++ public_html/epic/personas/suzy.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<title>Suzy</title>
+	</head>
+	<body>
+		<h1>Persona</h1>
+		<h2>Suzy</h2>
+		<p>
+		Suzy is the manager of a large firm that sends small teams to several physical locations
+		daily to preform a service. Each team requires a specialist A and a specialist B, an
+		employee can ether be a specialist A, specialist B, or an apprentice. And there must be a
+		1 to 1 ratio fo specialists to apprentices.  Managing over 50 employees, and a schedule of
+		over 10 locations ever day, Suzy finds her current scheduling solution doesn't cut it.
+		</p>
+	</body>
+</html>
