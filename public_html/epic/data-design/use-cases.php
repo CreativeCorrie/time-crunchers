f3addc9812830dcd2a524dae2e73b76a2b240d1d
Elaine Thomas <enajera2@cnm.edu>
2016-01-27 14:44:14 -0700

    adding use case for Talia and creating framework for other personas to be plugged into

--- public_html/epic/data-design/use-cases.php
+++ public_html/epic/data-design/use-cases.php
@@ -6,9 +6,15 @@
 	</head>
 	<body>
 		<h2>Talia, 27, fast-food worker</h2>
+		<h3>Use Case 1</h3>
 		<ol>
+			<li>Using her smartphone Talia accesses the TimeCruncher website from a bookmark.
 			<li>On the landing page Talia signs in with the user name and password she created on her first day of work</li>
 			<li>Talia is securely logged into the employee portal</li>
+			<li>On the employee page, Talia can see her scheduled days</li>
+			<li>Talia selects the "request time off" button</li>
+			<li>The website returns a calendar grid with clickable time slots populated by the current schedule</li>
+			<li>Talia selects the hours that she wants to request off and hits the submit button and logs out</li>
 		</ol><br>
 
 		<h2>Bob</h2>
