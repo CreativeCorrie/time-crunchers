89f47fbe5825d9ecbe67ed78daf6350a9272e6f1
Elaine Thomas <enajera2@cnm.edu>
2016-01-27 14:28:32 -0700

    adding use case for Talia and creating framework for other personas to be plugged into

---
+++ public_html/epic/data-design/use-cases.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<title>Use Cases</title>
+	</head>
+	<body>
+		<h2>Talia, 27, fast-food worker</h2>
+		<ol>
+			<li>On the landing page Talia signs in with the user name and password she created on her first day of work</li>
+			<li>Talia is securely logged into the employee portal</li>
+		</ol><br>
+
+		<h2>Bob</h2>
+		<ol>
+			<li>some stuff</li>
+			<li>moar stuff</li>
+		</ol><br>
+
+		<h2>Some other guy</h2>
+		<ol>
+			<li>some stuff</li>
+			<li>moar stuff</li>
+		</ol><br>
+
+		<h2>This other guy</h2>
+		<ol>
+			<li>some stuff</li>
+			<li>moar stuff</li>
+		</ol><br>
+
+	</body>
+</html>
