8d4025a9a9bf8cabdff800e8a2a080a92e528539
Sam Chandler <samuelvanchandler@gmail.com>
2016-01-27 14:52:47 -0700

    personas plus use case

--- public_html/epic/personas/suzy.php
+++ public_html/epic/personas/suzy.php
@@ -12,7 +12,17 @@
 		daily to preform a service. Each team requires a specialist A and a specialist B, an
 		employee can ether be a specialist A, specialist B, or an apprentice. And there must be a
 		1 to 1 ratio fo specialists to apprentices.  Managing over 50 employees, and a schedule of
-		over 10 locations ever day, Suzy finds her current scheduling solution doesn't cut it.
+		over 10 locations ever day, Suzy finds her current scheduling solution doesn't cut it. She
+		needs some way to generate a schedule according to the above critera while accounting for
+			an employees availablity and preference. She would also like it to keep track of the
+			hours each employee works as well as their pay rate.
+		</p>
+		<h1>Use Case</h1>
+		<p>
+			Suzy will spend a half hour to an hour or so during here work week, generating and fine
+			tuning the schedule. She can now handle all shift trade request and time off requests
+			for her phone on her lunch break.
+
 		</p>
 	</body>
 </html>
