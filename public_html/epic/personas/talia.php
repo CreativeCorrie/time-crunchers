77ba775b76da8a1a63f9fde564e504e450e6e6b9
Elaine Thomas <enajera2@cnm.edu>
2016-01-27 14:02:42 -0700

    add new persona (in progress)

--- public_html/epic/personas/talia.php
+++ public_html/epic/personas/talia.php
@@ -7,15 +7,18 @@
 	<body>
 		<img src="http://placehold.it/100x100">
 		<h2>Talia, 27, fast-food worker</h2>
-		<p>Talia is a smartphone user and has an older laptop at home that she used to use for homework but now uses primarily for streaming video.  Generally, she uses her smartphone to access the Internet.
+		<p>Talia is a smartphone user and has an older laptop at home that she bought for doing homework but now uses primarily for streaming video.  Generally, she uses her smartphone to access the Internet.
 		</p>
 
 		<h3>Needs</h3>
-		<p>
-			Words on a screen
-		</p>
+		<ol
+			<li>Talia wants to access her work schedule anywhere/anytime</li>
+			<li>When Talia wants to request schedule time off without calling in or going into work.</li>
+			<li>Talia wants to volunteer to pick up shift without calling in or going in to work to put her name on a list.</li>
+		</ol>
+
 		<h3>Frustrations</h3>
-		<p>She likes her job well enough but she frequently has problems with the scheduling.  When she wants to make plans with her friends she has to drive to her workplace the Sunday before to find out when her manager has scheduled her.  Also, some stuff.
+		<p>She likes her job well enough but she frequently has problems with her work schedule.  When she wants to make plans with her friends she has to drive to her workplace the Sunday before to find out when her manager has scheduled her.  Also, some stuff.
 		</p>
 	</body>
 	</html>
