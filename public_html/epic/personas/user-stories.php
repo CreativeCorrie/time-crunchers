6a44a44534e45321d3179efe90468646ed313e9a
Elaine Thomas <enajera2@cnm.edu>
2016-01-27 15:04:28 -0700

    added two user stories

---
+++ public_html/epic/personas/user-stories.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<title>User Stories</title>
+	</head>
+	<body>
+		<h2>S1</h2>
+		<p>
+			An employee logs in to TimeCrunchers to to check and see what days they are working next week.
+		</p>
+
+		<h2>S2</h2>
+		<p>
+			A manager pulls up the schedule for next week and reviews time off requests and how many employees are already scheduled to work.
+		</p>
+	</body>
+</html>
