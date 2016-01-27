9325fc81803b2a8ccce9dad3e79b9471ac511e77
Sam Chandler <samuelvanchandler@gmail.com>
2016-01-27 14:14:15 -0700

    test

--- public_html/epic/personas/bob.php
+++ public_html/epic/personas/bob.php
@@ -1,1 +1,20 @@
-bob is a small business owner, he has 14 employers. T
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<title>Sam's persona</title>
+	</head>
+	<body>
+		<h1>Persona</h1>
+		<h2>Bob</h2>
+		<p>Bob is the the owner of a restaurant, he has 18 employees. He finds he spends
+			a lot of excess time on scheduling his employees' work shifts, accommodating their
+			schedule requests and preferences, keeping track of their hours, etc.
+		</p>
+			Suzy is the manager of a large firm that sends small teams to several physical locations
+			daily to preform a service. Each team requires a specialist A and a specialist B, an
+			employee can ether be a specialist A, specialist B, or an apprentice. And there must be a
+			1 to 1 ratio fo specialists to apprentices.  Managing over 50 employees, and a schedule of
+			over 10 locations ever day, Suzy finds her current scheduling solution doesn't cut it.
+	</body>
+</html>
