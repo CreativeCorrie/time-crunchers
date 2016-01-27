936a28a3bdbff066c5fed63e529de5a64ff6d98a
Elaine Thomas <enajera2@cnm.edu>
2016-01-27 15:29:38 -0700

    adding ERD drawing

---
+++ public_html/epic/data-design/erd.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<title>ERD</title>
+	</head>
+	<body>
+		<h2>Entity Relationship Diagram</h2>
+		<img src="timeCruncherErd.svg" width="500" height="400">
+	</body>
+</html>
