9b72eb296ea77f2ca6d32354ffa61cfbe1d44cc1
Victoria Hooker <vhooker@cnm.edu>
2016-01-28 06:01:08 -0700

    added Hugh Allen

---
+++ public_html/epic/personas/hugh-allen.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Document</title>
+	</head>
+	<body>
+		Hugh Allen<br>
+		Age: 32<br>
+		Education: Sc.M-Harvard<br>
+		Occupation: Statistician Entrepreneur<br>
+		Income: 120,000 annual<br>
+
+		<h2>Work/Life/Balance:</h2>
+		Hugh runs his own business as a head contractor with a team of 23 sub-contractors for his state's Department of Health bringing quality assurance to Vaccines for Children.<br>
+		He has to keep track of his own schedule and the vaccine sites he visits.<br>
+		Each contractor under him also has a schedule they make up on their own and are responsible for and he needs to know about their plans.<br>
+		There are plenty of meetings within The State and he needs to keep track of when to be where and what to bring.<br>
+		He also has a schedule for when he teaches nurses how to administer, keep track of and enter vaccine information into The State and CDC web-based databases.<br>
+		He was in the Peace Corp and has very close friends within W.H.O. and they visit when in the area. Occasionally and upon need he also travels abroad to meet with them.<br>
+		Hugh travels all over the southwest and is an avid rock climber and loves to visit the pueblos and any public events they are doing when he is in their area.<br>
+		On a personal note: he would like to add these to his schedule, so when he is visiting a VFC site he can take a side trip as well for the sake of enjoyment.<br>
+		Hugh pays his taxes and bills on time but needs to be "gently" reminded of when to do these unpleasant activities.<br>
+		He also has a very cute and busy girlfriend who is a professional of standing and sometimes it is hard to sync their calendars together. There is a lot
+		of texting and calls between the two just discussing when they might have time for dinner together.<br>
+
+
+		<h2>Goals:</h2>
+		To improve his productivity and abilities in handling the scheduling of himself, vaccine sites, colleagues, and sub-contractors.<br>
+		Being in the right place at the right time.<br>
+		Being able to have more of a work-life balance.<br>
+
+		<h2>Needs:</h2>
+		To quickly find helpful and accurate scheduling information when needed.<br>
+		To feel he isn't wasting his time.<br>
+		To take advantage of his down time and time between appaointments to enjoy life.<br>
+
+		<h2>Challenges:</h2>
+		Overwhelmed by the idea that he has to know everything about everybody and keep it all straight so he doesn't hurt anyone's tender ego.<br>
+		Worried about his competence in keeping up with new technology especially when it changes so fast and radically.<br>
+		Frustrated at times when he can't find or re-find things in his overcrowded calendar.<br>
+
+		<h2>Technology:</h2>:
+		Hugh runs with the latest iPhone, 3 laptops with the latest version of Windows and Microsoft Office and 1 tablet mostly for Netflix when he is away from home.
+
+		<h2>Story:</h2>
+		Hugh absolutely loves his job. He feels very fortunate to be where he is and with the experiences and adventure he has acquired over just 32 years.  He enjoys the challenges of finding answers to a wide variety of questions. He is always learning alongside his colleagues and staff and is deeply appreciative of the range of knowledge acquired in his daily work. In addition to checking vaccine quality, Hugh wears many other hats in his organization such as running the backend of a small business, following disease research and development, and teaching workshops.
+
+		Hugh is interested in how the CDC and The State keep top notch services to their communities. Unfortunately, Hugh is unable to follow cutting edge development as much as he would like. The majority of work is spent in public service in the field and he doesn't have time to enrich his knowledge base with a lot of personal research. It's often a decision between keeping his business going and exploring technological advancement. As expected, the former always wins out. Additionally, The State, the CDC and even his sub-contractors don't care how he gets it all done, just that he does.
+
+		Hugh worries about his business staying current and relevant in the perspective of his peers. He has heard of an awesome new web-based scheduling site called TimeCrunchers from one of his sub-contractors. He can see the advantages but remains cautious in his observations. He certainly doesn't have time to build such a thing himself and is resolved to keep an eye on the big developments of the TimeCrunchers team as he hears about them.
+
+	</body>
+</html>
