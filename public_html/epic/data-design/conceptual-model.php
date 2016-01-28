2b40848a85bbd084aec4f3ee71b811dcee6b1744
Elaine Thomas <enajera2@cnm.edu>
2016-01-28 16:25:58 -0700

    create conceptual model

---
+++ public_html/epic/data-design/conceptual-model.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Conceptual Model</title>
+	</head>
+	<body>
+		<h1>Conceptual Model</h1>
+		<ol>
+			<li>A company can have many users</li>
+			<li>Each user has an access level</li>
+			<li>A user can create a shift</li>
+			<li>Each user can have many schedules</li>
+			<li>Each schedule can have many users</li>
+			<li>A shift can be made into a schedule</li>
+		</ol>
+
+	</body>
+</html>
