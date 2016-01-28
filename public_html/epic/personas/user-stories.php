6ecd06d3dc85733ecb3384c22a56e2aa6484039a
DenzylFontaine <ddenzfong@gmail.com>
2016-01-28 13:29:38 -0700

    uses cases for talia, bob, hugh, and suzy

--- public_html/epic/personas/user-stories.php
+++ public_html/epic/personas/user-stories.php
@@ -5,14 +5,24 @@
 		<title>User Stories</title>
 	</head>
 	<body>
-		<h2>S1</h2>
+		<h2>Suzy</h2>
 		<p>
-			An employee logs in to the website to to check and see what days they are working next week.
+			Suzy has arrived at her location for the day but one of her employees is missing, she checks the Time Cruncher schedule to see who was supposed to work.
 		</p>
 
 		<h2>Hugh Allen</h2>
 		<p>
-			As a daily routine, Hugh likes to quickly check out his agenda and the agenda of all of his sub-contractors
+			As a daily routine, Hugh likes to quickly check out his agenda and the agenda of all of his sub-contractors.
+		</p>
+
+		<h2>Bob</h2>
+		<p>
+			Bobs phone just died, so he decides to use his desktop instead since the layout is almost identical.
+		</p>
+
+		<h2>Talia</h2>
+		<p>
+			Talia woke up later than usual she need to make sure that she doesn't work today, she checks the Time Cruncher app.
 		</p>
 
 		<h2>Hugh Allen</h2>
