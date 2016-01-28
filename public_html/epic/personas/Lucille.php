4432799dc719f555967f796b9ca33ded21eb895d
Sam Chandler <samuelvanchandler@gmail.com>
2016-01-28 12:46:36 -0700

    Created Lucille's persona and use case

---
+++ public_html/epic/personas/Lucille.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<title>Lucille</title>
+	</head>
+	<body>
+		<h1>Persona</h1>
+		<h2>Lucille</h2>
+		<p>
+			Lucille is a server at Bob's restaurant. One of the most frustrating parts of her job, aside
+			from the customers, is the scheduling abiguity. When she needs time off, she either texts Bob
+			or tells him in person when they happen to be at the restaurant at the same time. If she wants to
+			find someone to cover her shift, she mass texts all her co-workers. When the new
+			schedule comes out, she manually enters the shifts she is scheduled to work in to google calendars,
+			which is her preferred time management tool.
+		</p>
+		<h1>Use Case</h1>
+		<p>
+			Lucille primarily uses the mobile app on her iphone6.  She can check her schedule, offer her shifts
+			up trade, pick up shifts from other employees looking to switch, message and communicate with co-works
+			to coordinate with out involving management, as well as set her future availability preferences
+			and time off request.
+		</p>
+	</body>
+</html>
