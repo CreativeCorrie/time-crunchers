d80e0f90954c86cf8f293976c3c7c5d9ac1912d1
Sam Chandler <samuelvanchandler@gmail.com>
2016-01-28 12:04:19 -0700

    Fleshed out Suzy's persona and use case

--- public_html/epic/personas/suzy.php
+++ public_html/epic/personas/suzy.php
@@ -8,21 +8,25 @@
 		<h1>Persona</h1>
 		<h2>Suzy</h2>
 		<p>
-		Suzy is the manager of a large firm that sends small teams to several physical locations
-		daily to preform a service. Each team requires a specialist A and a specialist B, an
-		employee can ether be a specialist A, specialist B, or an apprentice. And there must be a
-		1 to 1 ratio fo specialists to apprentices.  Managing over 50 employees, and a schedule of
-		over 10 locations ever day, Suzy finds her current scheduling solution doesn't cut it. She
-		needs some way to generate a schedule according to the above critera while accounting for
-			an employees availablity and preference. She would also like it to keep track of the
-			hours each employee works as well as their pay rate.
+			Suzy is the manager of a large firm that sends small teams to several physical locations
+			daily to preform a service. Each team requires a specialist A and a specialist B, an
+			employee can ether be a specialist A, specialist B, or an apprentice. And there must be a
+			1 to 1 ratio fo specialists to apprentices.  Managing over 50 employees, and a schedule of
+			over 10 locations ever day, Suzy finds her current scheduling solution doesn't cut it. She
+			needs some way to generate a schedule according to the above criteria while accounting for
+			an employees availability and preference. She would also like it to keep track of the
+			hours each employee works as well as their pay rate. Since there is a geographic component
+			to task, she also thinks it would be helpful if the software could route the teams to
+			their.
 		</p>
 		<h1>Use Case</h1>
 		<p>
-			Suzy will spend a half hour to an hour or so during here work week, generating and fine
-			tuning the schedule. She can now handle all shift trade request and time off requests
-			for her phone on her lunch break.
-
+			Suzy will spend an hour or so during her work week, generating and fine tuning the schedule on her
+			office desktop. Employees can upload their schedule preferences and she can generate schedule designs,
+			that meet the criteria for the project, from the push of a button, as well generate maps to and from locations.
+			She can then distribute these schedules and maps by way of email or sync with an application like
+			google maps. Suzy rarely gets requests for schedule alterations, bun she can now handle all shift trade
+			request for her phone on her lunch break, using the mobile app.
 		</p>
 	</body>
 </html>
