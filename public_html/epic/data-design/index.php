5ae5fc7ce483b0dc3bcaccc56ab90298c257ac92
Victoria Hooker <vhooker@cnm.edu>
2016-01-29 12:28:05 -0700

    Put suzy's use case in the proper place.

---
+++ public_html/epic/data-design/index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+	<meta charset="UTF-8">
+	<head>
+
+		<title>Time Crunchers Index</title>
+	</head>
+	<body>
+		<h2>TimeCrunchers Timeline</h2>
+			<ul>
+				<li>
+					<a href="../time-crunchers-timeline.php"></a>
+				</li>
+
+			</ul>
+		<h2>Data Design</h2>
+			<ul>
+				<li>
+					<a href="conceptual-model.php">Conceptual Model</a>
+				</li>
+				<li>
+					<a href="erd.php">ERD</a>
+				</li>
+				<li>
+					<a href="system-goals.php">System Goals</a>
+				</li>
+				<li>
+					<a href="use-cases.php">Use Cases</a>
+				</li>
+			</ul>
+		<h2>Personas</h2>
+			<ul>
+				<li>
+					<a href="../personas/bob.php">Bob</a>
+				</li>
+				<li>
+					<a href="../personas/hugh-allen.php">Hugh Allen</a>
+				</li>
+				<li>
+					<a href="../personas/Lucille.php">Lucille</a>
+				</li>
+				<li>
+					<a href="../personas/suzy.php">Suzy</a>
+				</li>
+				<li>
+					<a href="../personas/talia.php">Talia</a>
+				</li>
+				<li>
+					<a href="../personas/user-stories.php">User Stories</a>
+				</li>
+			</ul>
+	</body>
+</html>
