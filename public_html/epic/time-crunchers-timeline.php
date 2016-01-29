c992b08466bfbe877e1f1f593e542d78b29854ea
Victoria Hooker <vhooker@cnm.edu>
2016-01-28 21:26:14 -0700

    Added Timeline

---
+++ public_html/epic/time-crunchers-timeline.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Time Line</title>
+	</head>
+	<body>
+		<h1>TimeCrunchers Crunched Time Line</h1>
+		<h2>Week 1- Jan 25th to Jan 29th</h2>
+			<p>Plan website needs. Decide plan of action.</p>
+		<h2>Week 2- Feb 1st to Feb 5th</h2>
+			<p>Rinse & Repeat</p>
+			<p>Depending on Bridge input.</p>
+			<p>Start mySQL</p>
+		<h2>Week 3- Feb 8th to Feb 12th</h2>
+			<p>Development of mySQL</p>
+			<p>Maybe page planning</p>
+		<h2>Week 4- Feb 15th to Feb 19th</h2>
+			<p>page planning</p>
+			<p>Maybe content</p>
+		<h2>Week 5- Feb 22th to Feb 26th</h2>
+			<p>Content</p>
+			<p>Maybe Soft launch</p>
+		<h2>Week 6- Feb 29th to March 4th</h2>
+			<p>Revision (Rinse and Repeat)</p>
+			<p>Final Lunch</p>
+		<h2>Week 7- March 7th to March 11th</h2>
+			<p>Presentation</p>
+	</body>
+</html>
