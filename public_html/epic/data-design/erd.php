469c09bbb1eaec86befac03b502d12c50f936711
Elaine Thomas <enajera2@cnm.edu>
2016-01-30 16:48:04 -0700

    added an alt tag to erd

--- public_html/epic/data-design/erd.php
+++ public_html/epic/data-design/erd.php
@@ -6,6 +6,6 @@
 	</head>
 	<body>
 		<h2>Entity Relationship Diagram</h2>
-		<img src="../images/besterd.svg" width="500" height="600">
+		<img src="../images/besterd.svg" alt="erd diagram" width="500" height="600">
 	</body>
 </html>
