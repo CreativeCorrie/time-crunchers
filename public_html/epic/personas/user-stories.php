348987422ba912ea50ea6a6d73226f769e2e9fbd
Elaine Thomas <enajera2@cnm.edu>
2016-01-30 12:33:03 -0700

    added a title to the page and changed the <h2 tags to <h3

--- public_html/epic/personas/user-stories.php
+++ public_html/epic/personas/user-stories.php
@@ -5,27 +5,28 @@
 		<title>User Stories</title>
 	</head>
 	<body>
-		<h2>Suzy</h2>
+		<h1>User Stories</h1>
+		<h3>Suzy</h3>
 		<p>
 			Suzy has received an email from a sick employee with a doctors not attached, now she needs to rework the next few days of the schedule.
 		</p>
 
-		<h2>Hugh Allen</h2>
+		<h3>Hugh Allen</h3>
 		<p>
 			As a daily routine, Hugh likes to quickly check out his agenda and the agenda of all of his sub-contractors.
 		</p>
 
-		<h2>Bob</h2>
+		<h3>Bob</h3>
 		<p>
 			Bob approved a shift switch but the worker has not arrived on time, he needs to check who this is.
 		</p>
 
-		<h2>Talia</h2>
+		<h3>Talia</h3>
 		<p>
 			Talia woke up later than usual she need to make sure that she doesn't work today, she checks the Time Cruncher app.
 		</p>
 
-		<h2>Hugh Allen</h2>
+		<h3>Hugh Allen</h3>
 		<p>
 			He sees a new entry marked in bold green signifying a meeting is wanted by someone and he needs to see if it fits in schedule and can add it easily.
 		</p>
