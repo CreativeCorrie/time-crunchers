fbf55cd2d75720da625019f1be0a9201bbb35219
DenzylFontaine <ddenzfong@gmail.com>
2016-02-01 15:50:44 -0700

    added the ipad use to talia's usecase 1

--- public_html/epic/personas/use-cases.php
+++ public_html/epic/personas/use-cases.php
@@ -29,7 +29,7 @@
 		<h2>Talia, 27, fast-food worker</h2>
 		<h3>Use Case 1</h3>
 		<ol>
-			<li>Using her smartphone Talia accesses the TimeCruncher website from a bookmark.
+			<li>Using her Ipad Talia accesses the TimeCruncher website from a bookmark.
 			<li>On the landing page Talia signs in with the user name and password she created on her first day of work</li>
 			<li>Talia is securely logged into the employee portal</li>
 			<li>On the main employee page, Talia can see the work schedule for the week</li>
@@ -40,13 +40,13 @@
 
 		<h3>Use Case 2</h3>
 		<ol>
-			<li>Using her laptop Talia accesses the TimeCruncher website from a bookmark.
+			<li>Using her laptop Talia accesses the TimeCruncher website from a bookmark.</li>
 			<li>On the landing page Talia signs in with the user name and password she created on her first day of work</li>
 			<li>Talia is securely logged into the employee portal</li>
-			<li>On the employee page, Talia can see the work schedule for the week</li>
-			<li>Talia selects the day she wants to volunteer for</li>
-			<li>The website returns a calendar grid and shows the timeslot is currently assigned to another employee</li>
-			<li>Talia selects the add your name to volunteer list</li>
+			<li>On the employee page, Talia can see the work schedule for the week.</li>
+			<li>Talia selects the day she wants to volunteer for.</li>
+			<li>The website returns a calendar grid and shows the timeslot is currently assigned to another employee.</li>
+			<li>Talia selects the add your name to volunteer list.</li>
 		</ol><br>
 
 		<h2>Hugh Allen</h2>
@@ -57,7 +57,6 @@
 			<li>He scrolls through each of his active sub-contractors to ensure there were no cancellations and the work flow for the day will go well</li>
 			<li>Within moments Hu is able to see where every one is and their agenda for the day.</li>
 			<li>If there is a cancellation it is marked in bold red so he knows in a glance and can follow up.</li>
-
 		</ol><br>
 
 		<h2>Hugh Allen</h2>
