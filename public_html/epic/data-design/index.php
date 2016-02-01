dcb7bd83209f5db23d39d905534aa2cf63de7b0c
Victoria Hooker <vhooker@cnm.edu>
2016-02-01 15:31:05 -0700

    Changed directory

--- public_html/epic/data-design/index.php
+++ public_html/epic/data-design/index.php
@@ -10,44 +10,44 @@
 		<h2>Epic</h2>
 			<ul>
 				<li>
-					<a href="data-design/time-crunchers-timeline.php">Timeline</a>
+					<a href="time-crunchers-timeline.php">Timeline</a>
 				</li>
 
 			</ul>
 		<h2>Data Design</h2>
 			<ul>
 				<li>
-					<a href="data-design/conceptual-model.php">Conceptual Model</a>
+					<a href="conceptual-model.php">Conceptual Model</a>
 				</li>
 				<li>
-					<a href="data-design/erd.php">ERD</a>
+					<a href="erd.php">ERD</a>
 				</li>
 				<li>
 					<a href="system-goals.php">System Goals</a>
 				</li>
 				<li>
-					<a href="use-cases.php">Use Cases</a>
+					<a href="../personas/use-cases.php">Use Cases</a>
 				</li>
 			</ul>
 		<h2>Personas</h2>
 			<ul>
 				<li>
-					<a href="personas/bob.php">Bob</a>
+					<a href="../personas/bob.php">Bob</a>
 				</li>
 				<li>
-					<a href="personas/hugh-allen.php">Hugh Allen</a>
+					<a href="../personas/hugh-allen.php">Hugh Allen</a>
 				</li>
 				<li>
-					<a href="personas/lucille.php">Lucille</a>
+					<a href="../personas/lucille.php">Lucille</a>
 				</li>
 				<li>
-					<a href="personas/suzy.php">Suzy</a>
+					<a href="../personas/suzy.php">Suzy</a>
 				</li>
 				<li>
-					<a href="personas/talia.php">Talia</a>
+					<a href="../personas/talia.php">Talia</a>
 				</li>
 				<li>
-					<a href="user-stories.php">User Stories</a>
+					<a href="../personas/user-stories.php">User Stories</a>
 				</li>
 			</ul>
 	</body>
