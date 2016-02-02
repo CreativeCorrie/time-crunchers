e6137679bcd2c02194c9f88b8323b8a5a03fc7af
Elaine Thomas <enajera2@cnm.edu>
2016-02-02 11:51:18 -0700

    rearrange the index to match class notes

--- public_html/epic/data-design/index.php
+++ public_html/epic/data-design/index.php
@@ -2,44 +2,37 @@
 <html lang="en">
 	<meta charset="UTF-8">
 	<head>
-
 		<title>Time Crunchers Index</title>
 	</head>
 	<body>
-		<h1>TimeCrunchers</h1>
-		<h2>Epic</h2>
-			<ul>
-				<li>
-					<a href="time-crunchers-timeline.php">Timeline</a>
-				</li>
-
-			</ul>
+		<h1>TimeCrunchers Epic</h1>
+		<ul>
+			<li><a href="system-goals.php">Executive Summary & System Goals</a></li>
+			<li><a href="../personas/user-stories.php">User Stories</a></li>
+			<li><a href="conceptual-model.php">Conceptual Model</a></li>
+			<li><a href="erd.php">ERD</a></li>
+		</ul>
 		<h2>Data Design</h2>
 			<ul>
-				<li>
-					<a href="conceptual-model.php">Conceptual Model</a>
-				</li>
-				<li>
-					<a href="erd.php">ERD</a>
-				</li>
-				<li>
-					<a href="system-goals.php">System Goals</a>
-				</li>
-				<li>
-					<a href="../personas/use-cases.php">Use Cases</a>
-				</li>
+				<li><a href="conceptual-model.php">Conceptual Model</a></li>
+				<li><a href="erd.php">ERD</a></li>
+				<li><a href="system-goals.php">System Goals</a></li>
 			</ul>
-		<h2>Personas</h2>
-			<ul>
-				<li>
-					<a href="../personas/suzy-hughes.php">Suzy Hughes</a>
-				</li>
-				<li>
-					<a href="../personas/talia.php">Talia</a>
-				</li>
-				<li>
-					<a href="../personas/user-stories.php">User Stories</a>
-				</li>
-			</ul>
+
+				<h2>Personas</h2>
+				<ul>
+					<li><a href="../personas/suzy-hughes.php">Suzy Hughes</a></li>
+					<li><a href="../personas/talia.php">Talia</a></li>
+				</ul>
+
+				<h2>Use Cases</h2>
+				<ul>
+					<li><a href="../personas/use-cases.php">Use Cases</a></li>
+				</ul>
+
+		<h2>Development Timeline</h2>
+		<ul>
+			<li><a href="time-crunchers-timeline.php">Timeline</a></li>
+		</ul>
 	</body>
 </html>
