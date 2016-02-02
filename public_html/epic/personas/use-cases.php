0a42638df2367e2f74d762e13bcf27dba6513fd4
Elaine Thomas <enajera2@cnm.edu>
2016-02-02 11:24:57 -0700

    removed unnecessary use cases

--- public_html/epic/personas/use-cases.php
+++ public_html/epic/personas/use-cases.php
@@ -5,27 +5,8 @@
 		<title>Use Cases</title>
 	</head>
 	<body>
-		<h2>Use Cases</h2>
-		<ul>
-			<li>An administrator creates, updates, or deletes a user by clicking
-				the respective create, update, or delete button from their profile
-				page.</li>
-			<li>An administrator generates by clicking a "generate schedule" button.</li>
-			<li>An administrator approves/rejects a user trade by click the
-				respective approve/reject button.</li>
-			<li>A user checks their schedule by accessing the site, it will be
-				displayed on their main page.</li>
-			<li>A user offers up a shift by clicking a "offer shift" button on
-				the shift day of their schedule.</li>
-			<li>A user views available shifts from their home page, in the
-			"offered shifts" list.</li>
-			<li>A user takes another users shift by click on the "pick up shift"
-			button displayed beside the available shift on their home page</li>
-			<li>A user messages another user by clicking on their name in the schedule
-			or clicking the "employees" tab, giving them a list of all users from
-			their company to select from.</li>
-			<li>A user clicks on the "inbox" tab to view messages</li>
-		</ul>
+		<h1>Use Cases</h1>
+
 		<h2>Talia, 27, fast-food worker</h2>
 		<h3>Use Case 1</h3>
 		<ol>
@@ -49,46 +30,7 @@
 			<li>Talia selects the add your name to volunteer list.</li>
 		</ol><br>
 
-		<h2>Hugh Allen</h2>
-		<h3>Use Case 1</h3>
-		<ol>
-			<li>Hugh logs onto the secure TimeCruncher website from his iPhone to see where his employees are for the day's work and to ensure all VFC sites are covered according to schedule.</li>
-			<li>He looks over his own schedule first and is confident in the items since they have been confirmed via sms to the VFC clinics client.</li>
-			<li>He scrolls through each of his active sub-contractors to ensure there were no cancellations and the work flow for the day will go well</li>
-			<li>Within moments Hu is able to see where every one is and their agenda for the day.</li>
-			<li>If there is a cancellation it is marked in bold red so he knows in a glance and can follow up.</li>
-		</ol><br>
-
-		<h2>Hugh Allen</h2>
-		<h3>Use Case 2</h3>
-		<ol>
-
-			<li>Hugh logs onto the secure TimeCruncher website from his iPhone and sees a notification from someone asking for an appointment.</li>
-			<li>With a click of a button he is able to see the details of the appointment request.</li>
-			<li>He is confident if he clicks the "accept" button, TimeCrunchers will notify if there is a conflict in the appoitment.</li>
-		</ol><br>
-
-		<h2>Bob</h2>
-		<h3>Use Case 1</h3>
-		<p>
-			Bob takes about 30 minutes each week to generate a schedule from his personal Mac Book, either
-			from home or the manager's office at the restaurant.
-			The schedules will take into account any schedule requests and preferences his staff have
-			submitted to the time crunchers data base. After the schedules are distributed, his employees
-			can coordinate amongst themselves in trading shifts, and he can utilize the mobile app, at his
-			own leisure to approve or disapprove the requests.
-		</p><br>
-
-		<h2>Lucille</h2>
-		<h3>Use Case 1</h3>
-		<p>
-			Lucille primarily uses the mobile app on her iphone6. She can check her schedule, offer her shifts
-			up trade, pick up shifts from other employees looking to switch, message and communicate with co-works
-			to coordinate with out involving management, as well as set her future availability preferences
-			and time off request.
-		</p>
-
-		<h2>Suzy</h2>
+		<h2>Suzy Hughes, 32, Manager</h2>
 		<h3>Use Case 1</h3>
 		<p>Suzy: accesses the TimeCrunchers site and enters her login information.<br>
 			Site: retrieves Suzy's account, displays her home page.<br>
