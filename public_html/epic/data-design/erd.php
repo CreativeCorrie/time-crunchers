e0f2cdb4f332ab70a33db8f12327657d6f74c6f2
Victoria Hooker <vhooker@cnm.edu>
2016-02-03 08:10:36 -0700

    made a small change so it would pull properly

--- public_html/epic/data-design/erd.php
+++ public_html/epic/data-design/erd.php
@@ -5,7 +5,7 @@
 		<title>E R D</title>
 	</head>
 	<body>
-		<h2>Entity Relationship Diagram</h2>
+		<h2>Entity Relationship Diagra</h2>
 		<img src="../images/ERD.svg" alt="erd diagram" width="500" height="600">
 	</body>
 </html>
