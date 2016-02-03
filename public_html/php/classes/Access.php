a4b60b643ca1caf39a8848d37317e90dfc4e96f8
DenzylFontaine <ddenzfong@gmail.com>
2016-02-03 14:46:16 -0700

    constructor, accessor and mutator completed for Access.php
    https://app.asana.com/0/84113647261174/86938698382972

--- public_html/php/classes/Access.php
+++ public_html/php/classes/Access.php
@@ -1,8 +1,7 @@
 <?php
 namespace Edu\Cnm\Timecrunchers;
 
-require_once("autoload.php");
-
+require_once("autoloader.php");
 /**
  * Access, is what is going to decide what actions you allowed to make on the site
  *
@@ -11,9 +10,9 @@ require_once("autoload.php");
  *@author Denzyl Fontaine
  **/
 
-class Access implements \JsonSerializable {
+class Access {
 	/**
-	 * id for Access is accessId ; this is the primary key
+	 * id for access is accessId ; this is the primary key
 	 * @var int $accessId
 	 */
 	private $accessId;
@@ -25,16 +24,16 @@ class Access implements \JsonSerializable {
 
 	/**
 	 * constructor
-	 * @param int|null $newAccessId of...
+	 * @param int|null $newAccessId of
 	 * @param string $newAccessName
 	 */
 public function __construct(int $newAccessId = null, string $newAccessName){
 	try{
 		$this->setAccessId($newAccessId);
 		$this->setAccessName($newAccessName);
-	} catch(\InvalidArgumentException $invalidArgment) {
+	} catch(\InvalidArgumentException $invalidArgument) {
 		//rethrow the exception to the caller
-		throw(new \InvalidArgumentException($invalidArgment->getMessage(), 0,$invalidArgment));
+		throw(new \InvalidArgumentException($invalidArgument->getMessage(), 0,$invalidArgument));
 	} catch(\RangeException $range) {
 		//rethrow exception to caller
 		throw(new \RangeException($range->getMessage(), 0, $range));
@@ -50,15 +49,15 @@ public function __construct(int $newAccessId = null, string $newAccessName){
 	 * @return int value of tweet id
 	 **/
 	public function getAccessId() {
-		return(this->accessId);
+		return($this->accessId);
 	}
 
 	/**
 	 * mutator method for access id
 	 *
-	 * @param int|null $newAccessId
+	 * @param int|null $newAccessId new value for access id
 	 * @throws \RangeException if $newAccessId is not postive
-	 * @throws \InvalidArgumentException if $newAccessId is not an integer
+	 * @throws \TypeError if $newAccessId is not an integer
 	 */
 	public function setAccessId(int $newAccessId = null) {
 		//first apply the filter to the input.
@@ -66,6 +65,48 @@ public function __construct(int $newAccessId = null, string $newAccessName){
 			$this->accessId = null;
 			return;
 		}
+
 		// verify the access id is positive
+		if($newAccessId <= 0) {
+			throw(new \RangeException("access id is not positive"));
+		}
+
+		//convert and store the tweet id
+		$this->accessId = $newAccessId;
+	}
+
+
+/**
+ * accessor method for accessName
+ *
+ * @return string of access name
+ */
+	public function getAccessName() {
+	return($this->accessName);
+}
+
+/**
+ * mutator method for access name
+ *
+ * @param string$newAccessName new value for access name
+ * @throws \InvalidArgumentException if new$AccessName is not a string or insecure
+ * @throws \RangeException if $newAccessName is >32
+ * @throws \TypeError if $newAccessName is not a string
+ */
+	public function setAccessName(string $newAccessName) {
+		//verify the access name is secure
+		$newAccessName = trim($newAccessName);
+		$newAccessName = filter_var($newAccessName, FILTER_SANITIZE_STRING);
+		if(empty($newAccessName) === true) {
+			throw(new \InvalidArgumentException("access name is empty or not secure"));
+		}
+
+		//verify the access name will fit in the database
+		if(strlen($newAccessName) > 32) {
+			throw(new \RangeException("access name too large"));
+		}
+
+		//store the access name
+		$this->accessName = $newAccessName;
 	}
 }
