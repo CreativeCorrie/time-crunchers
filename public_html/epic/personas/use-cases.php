af3b1309e45a923b8f2ea283f6ecb1ec23cb4816
Elaine Thomas <enajera2@cnm.edu>
2016-02-02 19:23:02 -0700

    adding technology to talia use case 2

--- public_html/epic/personas/use-cases.php
+++ public_html/epic/personas/use-cases.php
@@ -14,7 +14,7 @@
 			<li>On her laptop, she follows a link from the email invite her new employer sent her.</li>
 			<li>On the landing page Talia signs in with her email address and temporary password from the email.</li>
 			<li>The website asks Talia to set her own password.</li>
-			<li>Talia now has access to her crew schedule</li>
+			<li>Talia now has access to her crew schedule.</li>
 		</ol><br>
 		<h3>Use Case 2</h3>
 		<ol>
@@ -33,7 +33,7 @@
 			<li>On the employee page, Talia can see the work schedule for the week.</li>
 			<li>Talia selects the day she wants to request off.</li>
 			<li>The website returns a form with a comment box.</li>
-			<li>Talia adds the comment, need day off for jury duty.</li>
+			<li>Talia adds the comment, "need day off for jury duty."/li>
 			<li>After filling out the form, Talia submits her request.</li>
 		</ol><br>
 
@@ -41,10 +41,10 @@
 		<h3>Use Case 1</h3>
 		<p>Suzy: accesses the TimeCrunchers site and enters her login information.<br>
 			Site: retrieves Suzy's account, displays her home page.<br>
-			Suzy: clicks "employees" tab on her home page<br>
+			Suzy: clicks "employees" tab on her home page.<br>
 			Suzy: With a new employee application in hand, she navigates to the create new employee area.<br>
 			Suzy: Fills in the new employee's name, email and telephone number from the application.<br>
-			Suzy: clicks the "Invite New Employee" button
+			Suzy: clicks the "Invite New Employee" button.
 			Site: adds the employee to the database and sends an email invite to the employee to log into the Timecrunchers website to  create a password.<br>
 
 
@@ -55,19 +55,19 @@
 			Site: opens a window with a new schedule template<br>
 			Suzy: enters the name of the crew and the date range<br>
 			Site: Returns a new schedule<br>
-			Suzy: Creates shifts for the schedule with the dynamic calendar.<br>
+			Suzy: Creates shifts for the schedule with the dynamic calendar<br>
 			Suzy: When she is done she saves the new schedule<br>
 			Site: Enters the new schedule into the database<br>
 
 		<h3>Use Case 3</h3>
 		<ol>
 			<li>Suzy has received an alert on her iPhone for a new schedule request</li>
-			<li>Suzy follows the link from the alert back to the scheduling website.</li>
+			<li>Suzy follows the link from the alert back to the scheduling website</li>
 			<li>Suzy is directed to a secure log in page where she uses her credentials to log into her administrator account</li>
 			<li>After log in, Suzy is presented with a list of all pending schedule requests</li>
 			<li>Suzy is able to approve or deny the schedule change and also include a comment</li>
-			<li>Talia ticks the request time off box and adds the comment "jury duty".</li>
-			<li>After filling out the form, Talia submits her request.</li>
+			<li>Talia ticks the request time off box and adds the comment "jury duty"</li>
+			<li>After filling out the form, Talia submits her request</li>
 		</ol><br>
 	</body>
 </html>
