7c933b70b827c9a3ad971feac6dc7b61b00ffdf5
DenzylFontaine <ddenzfong@gmail.com>
2016-02-03 16:09:27 -0700

    began to create the constructor for User.php
    https://app.asana.com/0/84113647261174/86938698382970

--- public_html/php/classes/User.php
+++ public_html/php/classes/User.php
@@ -68,6 +68,34 @@ class User {
 	private $userSalt;
 
 	/**
+	 * @param int|null $newUserId id of this user or null if new user
+	 * @param int|null $newCompanyId id of the company or null if new user
+	 * @param int|null $newAccessId id of the access or null if new user
+	 * @param string $newUserPhone string containing user phone number
+	 * @param string $newUserFirstName string containing user first name
+	 * @param string $newUserLastName string containing user last name
+	 * @param int|null $newUserCrewId id of the crew or null if new crew
+	 * @param string $newUserEmail string containing user email
+	 * @param int|null $newUserActivation containing user activation info
+	 * @param int|null $newUserHash containing user password info
+	 * @param int|null $newUserSalt containing user password info
 	 *
+	 * @throws \InvalidArgumentException if the data types are not valid
+	 * @throws \RangeException if data values are out of bounds
+	 * @throws \TypeError if data types violate type hints
+	 * @thorws \Exception if some other exception occurs
 	 */
+
+	public function __construct(int $newUserId = null, int $newCompanyId = null, int $newAccessId = null, string $newUserPhone, string $newUserFirstName, string $newUserLastName, int $newUserCrewId = null, string $newUserEmail, int $newUserActivation = null, int $newUserHash = null, int $newUserSalt = null) {
+		{
+			try {
+				$this->setUserId($newUserId);
+				$this->setCompanyId($newCompanyId);
+				$this->setAccessId($newAccessId);
+				$this->setUserPhone($newUserPhone);
+				$this->setUserFirstName($newUserFirstName);
+				$this->setUserLastName($newUserLastName);
+			}
+		}
+	}
 }
