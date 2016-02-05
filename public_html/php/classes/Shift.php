26c03cdd94bf9fcc044f4baa03ee7f7539f14021
Victoria Hooker <vhooker@cnm.edu>
2016-02-05 12:26:40 -0700

    Started accessors and mutators to
     Shift.php
    https://app.asana.com/0/84113647261174/86938698382974

--- public_html/php/classes/Shift.php
+++ public_html/php/classes/Shift.php
@@ -24,17 +24,20 @@ class Shift {
 
 	/**
 	 * shiftCrewId, to identify the crew
+	 * @var int shiftCrewId
 	 * this is a foreign key
 	 **/
 	private $shiftCrewId;
 
 	/**
 	 * shiftTime, identifies the time of a shift
+	 * @var int shiftTime
 	 **/
 	private $shiftTime;
 
 	/**
 	 *shiftDay, identifies the day of shift
+	 * @var int shiftDay
 	 *this may be a timestamp?
 	 **/
 	private $shiftDay;
@@ -42,11 +45,13 @@ class Shift {
 	/**
 	 *shiftDeleted, identifies or an action for un-needed shifts
 	 * this is a soft delete
+	 * @var int shiftDeleted
 	 **/
 	private $shiftDeleted;
 
 	/**
 	 * shiftRequestId, identifies shift requests
+	 * @var int shiftRequestId
 	 * this is a foreign key
 	 **/
 	private $shiftRequestId;
@@ -56,5 +61,60 @@ class Shift {
 	 *
 	 * @return int value of shiftId
 	 **/
-	public f
+	public function getShiftId() {
+		return($this->shiftId);
+	}
+	/**
+	 * Mutator method for shift id
+	 *
+	 * @param int $newShiftId new value of shift id
+	 * @throws UnexpectedValueException if $newShiftId is ot an integer
+	 **/
+	public function setShiftId($newShiftId) {
+		//verify the profile id is valid
+		$newShiftId = filter_var($newShiftId, FILTER_VALIDATE_INT);
+		if($newShiftId === false) {
+			throw(new UnexpectedValueException("shift id is not a valid integer"));
+		}
+		//convert and store the shift id
+		$this->shiftId = intval($newShiftId);
+	}
+	/**
+	 * accessor method for shift user id
+	 *
+	 * @return int value of shift user id
+	 */
+	public function getShiftUserId() {
+		return($this->shiftUserId);
+	}
+
+	/**mutator method for shift user id
+	 *
+	 * @param int $newShiftUserId new value of shift user id
+	 * @throws UnexpectedValueException if $newShiftUserId is not an integer
+	 **/
+	public function setShiftUserId($newShiftUserId) {
+		//verify the shift user id is valid
+		$newShiftUserId = filter_var($newShiftUserId, FILTER_VALIDATE_INT);
+		if($newShiftUserId === false) {
+			throw(new UnexpectedValueException("user id is not a valid integer"));
+		}
+		//convert and store the shift user id
+		$this->shiftUserId = intval($newShiftUserId);
+	}
+	/**
+	 * assessor method for shift crew id
+	 *
+	 * @param int $newShiftCrewId new value of shift crew id
+	 * @throws UnexpectedValueException if $newShiftCrewId is not an integer
+	 **/
+	public function setShiftCrewId($newShiftCrewId) {
+		//verify the shift crew id is valid
+		$newShiftCrewId = filter_var($newShiftCrewId, FILTER_VALIDATE_INT);
+		if($newShiftCrewId === false) {
+			throw (new UnexpectedValueException("shift crew id is not a valid integer"));
+		}
+		//convert and store the profile id
+		$this->shiftCrewId = intval($newShiftCrewId);
+	}
 }
