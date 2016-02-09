36ae05c330096ed6fcaded15d0c6d9c15be6406b
Victoria Hooker <vhooker@cnm.edu>
2016-02-09 09:17:16 -0700

    Added the autoloader and fixed syntax error "\""
    Crew.php
    https://app.asana.com/0/84113647261174/86938698382976

--- public_html/php/classes/Crew.php
+++ public_html/php/classes/Crew.php
@@ -71,13 +71,13 @@ require_once ("autoloader.php");
 	 * Mutator method for cew id
 	 *
 	 * @param int $newCrewId new value of crew id
-	 * @throws UnexpectedValueException if $newCrewId is not an integer
+	 * @throws \UnexpectedValueException if $newCrewId is not an integer
 	 **/
 	public function setCrewId(int $newCrewId) {
 		//verify the course id is valid
 		$newCrewId = filter_var($newCrewId, FILTER_VALIDATE_INT);
 		if($newCrewId === false) {
-			throw(new UnexpectedValueException("crew id is not a valid integer"));
+			throw(new \UnexpectedValueException("crew id is not a valid integer"));
 		}
 		//convert and store the crew id
 		$this->crewId = intval($newCrewId);
@@ -93,8 +93,8 @@ require_once ("autoloader.php");
 	/**
 	 * Mutator method for location
 	 * @param string $newCrewLocation new value of copy
-	 * @throws InvalidArgumentException if copy is only non sanitized values
-	 * @throws RangeException if copy will not fit in the database
+	 * @throws \InvalidArgumentException if copy is only non sanitized values
+	 * @throws \RangeException if copy will not fit in the database
 	 * @throws \TypeError if $newCrewLocation is not a string
 	 **/
 	public function setCrewLocation(string $newCrewLocation) {
@@ -103,11 +103,11 @@ require_once ("autoloader.php");
 		$newCrewLocation = filter_var($newCrewLocation, FILTER_SANITIZE_STRING);
 		//Exception if only non-sanitized values
 		if($newCrewLocation === false) {
-			throw(new InvalidArgumentException("location is not secure"));
+			throw(new \InvalidArgumentException("location is not secure"));
 		}
 		//Exception if input will not fit int he database
 		if(strlen($newCrewLocation) > 255) {
-			throw(new RangeException("content is too large"));
+			throw(new \RangeException("content is too large"));
 		}
 		//convert and store the location
 		$this->crewLocation = $newCrewLocation;
@@ -124,13 +124,13 @@ require_once ("autoloader.php");
 	 * mutator method for crew company i
 	 *
 	 * @param int $newCrewCompanyId new value of crew company id
-	 * @throws UnexpectedValueException if $newCrewCompanyId is not an integer.
+	 * @throws \UnexpectedValueException if $newCrewCompanyId is not an integer.
 	 **/
 	public function setCrewCompanyId($newCrewCompanyId) {
 		//verify the profile id is valid
 		$newCrewCompanyId = filter_var($newCrewCompanyId, FILTER_VALIDATE_INT);
 		if($newCrewCompanyId === false) {
-			throw(new UnexpectedValueException("crew company id is not a valid integer"));
+			throw(new \UnexpectedValueException("crew company id is not a valid integer"));
 		}
 		//convert and store the profile id
 		$this->crewCompanyId = intval($newCrewCompanyId);
@@ -231,7 +231,7 @@ require_once ("autoloader.php");
 			if($row !== false) {
 				$crew = new Crew($row["crewId"], $row["crewLocation"], $row["crewCompanyId"]);
 				}
-			} catch(Exception $exception) {
+			} catch(\Exception $exception) {
 				//if the row couldn't be converted, rethrow it
 				throw(new \PDOException($exception->getMessage(), 0, $exception));
 			}
