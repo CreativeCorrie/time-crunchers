0000511f2cc1b125edd230637644c5469110a4e5
DenzylFontaine <ddenzfong@gmail.com>
2016-02-09 22:59:34 -0700

    started to work on userTest.php

--- public_html/test/userTest.php
+++ public_html/test/userTest.php
@@ -4,3 +4,32 @@ namespace Edu\Cnm\Timecrunchers\test;
 use Edu\Cnm\Timecrunchers\{UserId, User};
 
 //grab test parameters
+require_once("TimecrunchersTest.php");
+
+//grab the class under scrutiny
+require_once(dirname(__DIR__) . "/php/classes/autoloader.php");
+
+/**
+ * Full PHPUnit test for the user class
+ *
+ * This is a complete PHPUnit test of the user class. It is complete because *ALL* mySQL/PDO enabled methods
+ * are tested for both invalid and valid inputs.
+ *
+ * @see user
+ * @author Denzyl Fontaine>
+ **/
+class UserTesT extends TimecrunchersTest {
+	/**
+	 * content of user
+	 * @var string $VALID_USERCONTENT
+	 */
+	protected $VALID_USERCONTENT = "PHPUnit test passing";
+	/**
+	 * content of updated user
+	 * @var string $VALID_USERCONTENT2
+	 */
+	protected $VALID_USERCONTENT2 = "PHPUnit test still passing";
+	/**
+	 * profile that created the user
+	 */
+}
