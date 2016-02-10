3fcd5d46613d22528400e56c0e061edd966c7005
Victoria Hooker <vhooker@cnm.edu>
2016-02-10 16:59:17 -0700

    added more to the crew test
    crewTest.php
    https://app.asana.com/0/84113647261174/86938698382976

--- public_html/test/CrewTest.php
+++ public_html/test/CrewTest.php
@@ -2,6 +2,7 @@
 namespace Edu\Cnm\Timecrunchers\Test;
 
 use Edu\Cnm\Timecrunchers\Company;
+use Edu\Cnm\Timecrunchers\Crew;
 
 //grab the project test parameters
 require_once ("TimecrunchersTest.php");
@@ -19,15 +20,15 @@ require_once(dirname(__DIR__) . "php/classes/autoloader.php");
  **/
 class CrewTest extends TimecrunchersTest {
 	/**
-	 * content of the Crew
-	 * @var string $VALID_CREWCONTENT
+	 * location of the Crew
+	 * @var string $VALID_CREWLOCATION
 	 **/
-	PROTECTED $VALID_CREWCONTENT = "PHPUnit test passing";
+	PROTECTED $VALID_CREWLOCATIONT = "PHPUnit test passing";
 	/**
-	 * content of the updated Crew
-	 * @var string $VALID_CREWCONTENT2
+	 * location of the updated Crew
+	 * @var string $VALID_CREWLOCATION2
 	 **/
-	protected  $VALID_CREWCONTENT2 = "PHPUnit tes still passing";
+	protected  $VALID_CREWLOCATION2 = "PHPUnit tes still passing";
 	/**
 	 * timestamp of the Crew; this starts as nuLL and is assigned Later
 	 * @var \DateTime $VALID_CREWDATE
@@ -47,6 +48,41 @@ class CrewTest extends TimecrunchersTest {
 		parent::setUp();
 
 		//create and insert a Company to own the test Crew
-		$this->company = new Company(null, "@phpunit", "test@phpunit.de" )
+		$this->company = new Company(null, "@phpunit", "test@phpunit.de", "+12125551212");
+		$this->company->insert($this->getPDO());
+
+		//calculate the date(just use the time the unit test was setup...)
+		$this->VALID_CREWDATE = new  \DateTime();
 	}
+
+	/**
+	 * test inserting a valid Crew and verify that the actual mySQL date matches
+	 **/
+	public  function testInsertValidCrew() {
+		//count the number of rows and save it for later
+		$numRows = $this->getConection()->getRowCount("crew");
+
+		//create a new Crew and insert to into mySQL
+		$crew->insert($this->getPDO());
+
+		//grab the data from mySQL and enforce the fields match our expectations
+		$pdoCrew = Crew::getCrewByCrewId($this->getPDO(), $crew->>getCrewId());
+		$this->assertEquals($numRows +1, $this->getConnection()->getRowCount("crew"));
+		$this->assertEquals($pdoCrew->getCompanyId(), $this->company->getCompanyId());
+		$this->assertEquals($pdoCrew->getCrewLocation(), $this->VALID_CREWLOCATION);
+	}
+	/**
+	 * test inserting a Crew that already exists
+	 *
+	 * @expectedException PDOException
+	 **/
+	public function testInsertInvalidCrew() {
+		//create a Crew with anon null crew id and watch it fail
+		$crew = new Crew(TimecrunchersTest::INVALID_KEY, $this->company->getCompanyId(), $this->VALID_CREWLOCATIONT, $this->VALID_CREWDATE);
+		$crew->insert($this->getPDO());
+	}
+
+	/**
+	 * test inserting a Crew, editing it and then updating it
+	 **/
 }
