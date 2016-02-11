c3a1946743f428dd6e3884f73ed9c9d6a4f81522
Victoria Hooker <vhooker@cnm.edu>
2016-02-11 11:16:20 -0700

    Did stuff forgot what Shift.php https://app.asana.com/0/84113659891169/86938698382974

--- public_html/test/ShiftTest.php
+++ public_html/test/ShiftTest.php
@@ -9,3 +9,19 @@ require_once ("TimecrunchersTest.php");
 
 //grab the class under scrutiny
 require_once(dirname(__DIR__) . "php/classes/autoloader.php");
+
+/**
+ * Full PHPUnit test for the Shift class
+ *
+ * This is a compplete PHPUnit tes of the Shift class. It is complee because *ALL* mySQL/PDO enabled methods
+ * are tested for both invalid and valid inputs.
+ *
+ * @see Shift
+ * @@author Corrie Hooker<creativecorrie@gmail.com> <Team Collaboration: TimeCrunchers>
+ **/
+class ShiftTest extends TimecrunchersTest {
+	/**
+	 * content of the Shift
+	 * @var string $VALID_
+	 **/
+}
