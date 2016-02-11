b46b9c6f8b6251d677527a97f2f4cd43f519826e
DenzylFontaine <ddenzfong@gmail.com>
2016-02-11 10:20:02 -0700

    testing Delete valid user
     https://app.asana.com/0/84113647261174/86938698382970

--- public_html/test/UserTest.php
+++ public_html/test/UserTest.php
@@ -2,6 +2,11 @@
 namespace Edu\Cnm\Timecrunchers\test;
 
 use Edu\Cnm\Timecrunchers\{Company, Access, Crew};
+use Edu\Cnm\Timcrunchers\Test\DataDesignTest;
+
+$password = "abc123";
+$salt = bin2hex(random_bytes(16));
+$hash = hash_pbkdf2("sha512", $password, $salt, 262144);
 
 //grab test parameters
 require_once("TimecrunchersTest.php");
@@ -18,7 +23,7 @@ require_once(dirname(__DIR__) . "/php/classes/autoloader.php");
  * @see user
  * @author Denzyl Fontaine>
  **/
-class UserTesT extends TimecrunchersTest {
+class UserTest extends TimecrunchersTest {
 	/**
 	 * content of user
 	 * @var string $VALID_USERCONTENT
@@ -43,8 +48,78 @@ class UserTesT extends TimecrunchersTest {
 		parent::setUp();
 
 		//create and insert a profile to own the test user
-		$this->user = new Profile(null,  "@phpunit", "test@phpunit.de", "+12125551212");
+		$this->user = new User(null,  "@phpunit", "test@phpunit.de", "+12125551212");
+		$this->user->insert($this->getPDO());
 	}
 
+	/**
+	 * test inserting a valid user and verify that the actual mySQL data matches
+	 **/
+	public function testInsertValidUser() {
+		//count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("user");
+
+		//create a new user and insert it into mySQL
+		$user = new User(null, $this->user->getUserId(), $this->VALID_USERFIRSTNAME);
+		$user->insert($this->getPDO());
+
+		//grab the data from mySQL and enforce the fields match our expectation
+		$pdoUser = User::getUserByUserId($this->$PDO(), $user->getUserId());
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("user"));
+		$this->assertEquals($pdoUser->getUserId(), $this->user->getUserId());
+		$this->assertEquals($pdoUser->getUserFirstName(), $this->VALID_USERFIRSTNAME);
+	}
+
+	/**
+	 * test inserting a User that already exists
+	 *
+	 * @expectedException PDOException
+	 **/
+	public function testInsertInvalidUser() {
+		//create new user with a null user Id and watch it fail
+		$user = new User(DataDesignTest::INVALID_KEY, $this->user->getUserId(), $this->VAL_USERFIRSTNAME);
+		$user->$this->insert($this->getPDO());
+	}
+
+	/**
+	 * test inserting a User, editing it, and then updating it
+	 **/
+	public function testDeleteValidUser() {
+		//count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("user");
+
+		//create a new user and insert it into mySQL
+		$user = new User(null, $this->user->getUserId(), $this->VALID_USERFIRSTNAME);
+		$user->insert($this->getPDO());
+
+		//edit the user and update it in mySQL
+		$user->setUserFirstName($this->VALID_USERFIRSTNAME2);
+		$user->update($this->PDO());
+
+		// grab the data from mySQL and enforce the fields match our expectations
+		$pdoUser = User::getUserbyUserId($this->getPDO(), $user->getUserId());
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("tweet"));
+		$this->assertEquals()
+
+
+
+
+
+
+
+
+
+
+
+
+		//delete user from mySQL
+		$this->assertEquals($numRows + 1, $getConnection()->getRowCount("user"));
+		$user->delete($this->getPDO());
+
+		//grab the data from mySQL and enforce the User does not exist
+		$pdoUser = User::getUserByUserId($this->getPDO(), $user->UserId());
+		$this->assertNull($pdoUser);
+		$this->assertEquals($numRows, $this->getConnection()->getRowCount("user"));
+	}
 
 }
