062c9cd3aa87fe5b71aec3c9889f3cff282ee261
Victoria Hooker <vhooker@cnm.edu>
2016-02-18 20:21:07 -0700

    wrote "getAllShifts" in shift class. shift.php https://app.asana.com/0/84113647261174/86938698382974

--- public_html/php/classes/Shift.php
+++ public_html/php/classes/Shift.php
@@ -509,6 +509,36 @@ class Shift implements \JsonSerializable {
 	}  // getShiftByDateRange
 
 	/**
+	 * gets all Shifts
+	 *
+	 * @param \PDO $pdo PDO connection object
+	 * @return \SplFixedArray SplFixedArray of Shifts found or null if not found
+	 * @throws \PDOException when mySQL related errors occur
+	 * @throws \TypeError when variables are not the correct data type
+	 **/
+	public static function getAllShifts(\PDO $pdo) {
+		// create query template
+		$query = "SELECT shiftId, shiftUserId, shiftCrewId, shiftRequestId, shiftStartTime, shiftDuration, shiftDate, shiftDelete FROM shift";
+		$statement = $pdo->prepare($query);
+		$statement->execute();
+
+		// build an array of shifts
+		$shifts = new \SplFixedArray($statement->rowCount());
+		$statement->setFetchMode(\PDO::FETCH_ASSOC);
+		while(($row = $statement->fetch()) !== false) {
+			try {
+				$shift = new shift($row["shiftId"], $row["shiftUserId"], $row["shiftCrewId"], $row["shiftRequestId"], $row["shiftStartTime"], $row["shiftDuration"], $row["shiftDate"], $row["shiftDelete"]);
+				$shifts[$shifts->key()] = $shift;
+				$shifts->next();
+			} catch(\Exception $exception) {
+				// if the row couldn't be converted, rethrow it
+				throw(new \PDOException($exception->getMessage(), 0, $exception));
+			}
+		}
+		return ($shifts);
+	}
+
+	/**
 	 * formats the state variables for JSON serialization
 	 *
 	 * @return array resulting state variables to serialize
