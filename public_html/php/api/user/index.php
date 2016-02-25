f60a22a0349057786eb37a688d34878627bf0834
DenzylFontaine <ddenzfong@gmail.com>
2016-02-25 16:31:49 -0700

    adding get functions for user api
    https://app.asana.com/0/84113647261174/92207042205624

--- public_html/php/api/user/index.php
+++ public_html/php/api/user/index.php
@@ -45,16 +45,16 @@ try {
 		throw(new InvalidArgumentException("id cannot be negative or empty", 405));
 	}
 	//sanitize and trim other fields
-	userCompanyId = filter_input(INPUT_GET, userCompanyId, FILTER_VALIDATE_INT);
-	userCrewId = filter_input(INPUT_GET, userCrewId, FILTER_VALIDATE_INT);
-	userAccessId = filter_input(INPUT_GET, userCrewId, FILTER_VALIDATE_INT);
-	userPhone = filter_input(INPUT_GET, userPhone, FILTER_SANITIZE_STRING);
-	userFirstName = filter_input(INPUT_GET, userFirstName, FILTER_SANITIZE_STRING);
-	UserLastName = filter_input(INPUT_GET, userLastName, FILTER_SANITIZE_STRING);
-	userEmail = filter_input(INPUT_GET, userEmail, FILTER_SANITIZE_STRING);
-	userActivation = filter_input(INPUT_GET, userActivation, FILTER_SANITIZE_STRING);
-	userHash = filter_input(INPUT_GET, userActivation, FILTER_SANITIZE_STRING);
-	userSalt = filter_input(INPUT_GET, userSalt, FILTER_SANITIZE_STRING);
+	userCompanyId = filter_input(INPUT_GET, "userCompanyId", FILTER_VALIDATE_INT);
+	userCrewId = filter_input(INPUT_GET, "userCrewId", FILTER_VALIDATE_INT);
+	userAccessId = filter_input(INPUT_GET, "userAccessId", FILTER_VALIDATE_INT);
+	userPhone = filter_input(INPUT_GET, "userPhone", FILTER_SANITIZE_STRING);
+	userFirstName = filter_input(INPUT_GET, "userFirstName", FILTER_SANITIZE_STRING);
+	UserLastName = filter_input(INPUT_GET, "userLastName", FILTER_SANITIZE_STRING);
+	userEmail = filter_input(INPUT_GET, "userEmail", FILTER_SANITIZE_STRING);
+	userActivation = filter_input(INPUT_GET, "userActivation", FILTER_SANITIZE_STRING);
+	userHash = filter_input(INPUT_GET, "userActivation", FILTER_SANITIZE_STRING);
+	userSalt = filter_input(INPUT_GET, "userSalt", FILTER_SANITIZE_STRING);
 
 	//handle rest calls, while only allowing admins to access database-modifying methods
 	if($method === GET) {
@@ -64,7 +64,61 @@ try {
 		//get the user based on the given
 		if(empty($id) === false) {
 			$user = User::getUserByUserId($pdo, $id);
-			if(user !== null && $_)
+			if(user !== null && $user->getAccessId() === $_SESSION["access"]->getAccessId()) {
+				$reply->data = $user;
+			}
+		} else if(empty )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+				if($method === "POST" || $method === "POST") {
+					verifyXsrf();
+					$requestContent = file_get_contents("php://input");
+					$requestObject = json_decode($requestContent);
+
+					//make sure all fields are present, in order to prevent database issues
+					if(empty($requestObject->userCompanyId) === true) {
+						throw(new InvalidArgumentException ("userCompanyId cannot be empty", 405));
+					}
+					if(empty($requestObject->userAccessId) === true) {
+						throw(new InvalidArgumentException ("userAccessId cannot be empty", 405));
+					}
+					if(empty($requestObject->userCrewId) === true) {
+						throw(new InvalidArgumentException ("userCrewId cannot be empty", 405));
+					}
+					if(empty($requestObject->userPhone) === true) {
+						throw(new InvalidArgumentException ("userPhone cannot be empty", 405));
+					}
+					if(empty($requestObject->userFirstName) === true) {
+						throw(new InvalidArgumentException ("userFirstName cannot be empty", 405));
+					}
+					if(empty($requestObject->userLastName) === true) {
+						throw(new InvalidArgumentException ("userLastName cannot be empty", 405));
+					}
+					if(empty($requestObject->userActivation) === true) {
+						throw(new InvalidArgumentException ("userActivation cannot be empty", 405));
+					}
+					if(empty($requestObject->userHash) === true) {
+						throw(new InvalidArgumentException ("userHash cannot be empty", 405));
+					}
+					if(empty($requestObject->userSalt) === true) {
+						throw(new InvalidArgumentException ("userSalt", 405));
+					}
+
+				}
+			}
 		}
 	}
 
