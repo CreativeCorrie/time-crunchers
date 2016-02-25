3f04169ec7bfda43f446b83379106d21d9fc31d5
DenzylFontaine <ddenzfong@gmail.com>
2016-02-25 13:18:50 -0700

    sanitized inputs for user api
    https://app.asana.com/0/84113647261174/92207042205624

--- public_html/php/api/user/index.php
+++ public_html/php/api/user/index.php
@@ -27,5 +27,30 @@ $reply->data = null;
 
 try {
 	//grab the sql connection
-	$pdo = connect to the
+	$pdo = connecToEncrytedMySQL();
+
+	//if the user session is empty, user is not logged in, throw an exception
+	if(empty($_session["volunteer"]) === true) {
+		setXsrfCookie("/");
+		throw(new RuntimeException("please login and set up", 401));
+	}
+
+	//determine which http has used
+	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
+
+	//sanitize inputs
+	$id = filter_input(INPUT_GET, "ID", FILTER_VALIDATE_INT);
+	//make sure the id is valid for methods that require it
+	if(($method === "DELETE" ||$method === "PUT") && (empty($id) === true || $id < 0)) {
+		throw(new InvalidArgumentException("id cannot be negative or empty", 405));
+	}
+	//sanitize and trim other fields
+	userCompanyId = filter_input(INPUT_GET, userCompanyId, FILTER_VALIDATE_INT);
+	userCrewId = filter_input(INPUT_GET, userCrewId, FILTER_VALIDATE_INT);
+	userAccessId = filter_input(INPUT_GET, userCrewId, FILTER_VALIDATE_INT);
+	userPhone = filter_input(INPUT_GET, userPhone, FILTER_SANITIZE_STRING);
+	userFirstName = filter_input(INPUT_GET, userFirstName, FILTER_SANITIZE_STRING);
+	UserLastName = filter_input(INPUT_GET, userLastName, FILTER_SANITIZE_STRING);
+	userEmail = filter_input(INPUT_GET, userEmail, FILTER_SANITIZE_STRING);
+
 }
