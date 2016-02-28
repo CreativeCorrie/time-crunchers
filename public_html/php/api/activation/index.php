1dbadf4248db15e51184761d6a99f0ba9f0d9d76
DenzylFontaine <ddenzfong@gmail.com>
2016-02-27 19:52:20 -0700

    started to add the micro apis

---
+++ public_html/php/api/activation/index.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once dirname(dirname(__DIR__)) . "/classes/autoloader.php";
+require_once dirname(dirname(__DIR__)) . "/lib/xsrf.php";
+require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
+use Edu\Cnm\Timecrunchers\User;
+
+/**
+ * controller/api for activation
+ *
+ * @author Denzyl Fontaine
+ */
+
+
