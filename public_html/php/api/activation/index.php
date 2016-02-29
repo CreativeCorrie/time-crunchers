403fab9bb308dd3557067b00bb1a541ddfb08cbc
DenzylFontaine <ddenzfong@gmail.com>
2016-02-29 12:15:02 -0700

    activation micro api completed

--- public_html/php/api/activation/index.php
+++ public_html/php/api/activation/index.php
@@ -11,23 +11,49 @@ use Edu\Cnm\Timecrunchers\Activation;
  * @author Denzyl Fontaine
  */
 
-try {
-	//Handle REST calls
-	if($method === "GET") {
-		//Set xsrf cookie
-		setXsrfCookie("/");
-
-		//Get Company based on given field
-		if(empty($id) === false) {
-			$activation = Activation::getUserActivationByUserActivation($pdo, $id);
-			if($activation !== null) {
-				$reply->data = $activation;
-			}
-		} else {
-			$activations = Activation::getAllActivations($pdo);
-			if($activations !== null) {
-				$reply->data = $activations;
-			}
-		}
-	}
+//verify the xsrf challenge
+if(session_status() !== PHP_SESSION_ACTIVE) {
+	session_start();
 }
+
+//prepare an empty reply
+$reply = new stdClass();
+$reply->status = 200;
+$reply->data = null;
+
+try {
+	//Grab MySQL connection
+	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/time-crunchers.ini");
+	//determine which http method was used
+
+	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
+
+
+	//handle REST calls, while allowing administrators to access database modifying methods
+	if($method === "GET") {
+		//set Xsrf cookie
+		setXsrfcookie("/");
+	}
+
+		//get the Activation based on the given field
+		$emailActivation = filter_input(INPUT_GET, "emailActivation", FILTER_VALIDATE_STRING);
+		$email = filter_input(INPUT_GET, "employeeEmail", FILER_VALIDATE_EMAIL);
+
+		$user = User::getUserByUserEmail($pdo, $email);
+
+		if($emailActivation !== $user->getUserActivation) {
+			throw(new \InvalidArgumentException ("activation code does not match"));
+		}
+
+		$user->setEmailActivation(NULL);
+		$user->update();
+		$reply->message = "successful activation!";
+
+	}
+catch
+	(Exception $exception) {
+		$reply->status = $exception->getCode();
+		$reply->message = $exception->getMessage();
+
+	}
+echo json_encode($reply);
