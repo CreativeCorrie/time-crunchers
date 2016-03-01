11122c74670a71c7eaaa33eac0b8d590c0dbb9a7
Elaine Thomas <enajera2@cnm.edu>
2016-03-01 12:54:14 -0700

    corrected captilization errors  in Shift API and fixed access if block
    https://app.asana.com/0/84113647261174/92207056979008

--- public_html/php/api/shift/index.php
+++ public_html/php/api/shift/index.php
@@ -36,13 +36,14 @@ try {
 	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
 
 	//sanitize inputs
-	$shiftid = filter_input(INPUT_GET, "Id", FILTER_VALIDATE_INT);
+	$shiftId = filter_input(INPUT_GET, "id", FILTER_VALIDATE_INT);
+
 	//make sure the id is valid for methods that require it
-	if(($method === "DELETE" || $method === "PUT") && (empty($shiftid) === true || $shiftid < 0)) {
+	if(($method === "DELETE" || $method === "PUT") && (empty($shiftId) === true || $shiftId < 0)) {
 		throw(new InvalidArgumentException("id cannot be empty or negative", 405));
 	}
-	//sanitize and trim the other fields
 
+	//sanitize and trim the other fields
 	$shiftUserId = filter_input(INPUT_GET, "shiftUserId", FILTER_VALIDATE_INT);
 	$shiftCrewId = filter_input(INPUT_GET, "shiftCrewId", FILTER_VALIDATE_INT);
 	$shiftRequestDate = filter_input(INPUT_GET, "shiftRequestId", FILTER_VALIDATE_INT);
@@ -60,7 +61,7 @@ try {
 		if(empty($id) === false) {
 			$shift = Shift::getShiftByShiftId($pdo, $id);
 			if($shift !== null && $shift->getShiftId() === $_SESSION["shift"]->getShiftId()) {
-				$reply->data = $crew;
+				$reply->data = $crew;  //TODO:not sure why this is $crew instead of $shift
 			}
 		} else if(empty($shiftUserId) === false) {
 			$shift = Shift::getShiftByShiftUserId($pdo, $id);
@@ -73,97 +74,92 @@ try {
 				$reply->data = $shift;
 			}
 		}
+
 		//	block non-admin users from doing admin-only tasks
 		if($method === "PUT") {
 			if(Access::isAdminLoggedIn() === true) {
-				// adminy thingz herez
-			} else {
-				throw(new RuntimeException("Must be an Administrator to access."));
-			}
-		}
+				if($method === "PUT" || $method === "POST") {
 
-		//if the session belongs to an admin, allow post, put and delete methods
-		if(empty($_SESSION["user"]) === false && $_SESSION["user"]->getUserIsAdmin() === true) {
+					// this is where we injected admin only abilities
+					verifyXsrf();
+					$requestContent = file_get_contents("php://input");
+					$requestObject = json_decode($requestContent);
 
-			if($method === "PUT" || $method === "POST") {
-
-				verifyXsrf();
-				$requestContent = file_get_contents("php://input");
-				$requestObject = json_decode($requestContent);
-
-				//make sure all fields are present, in order to prevent database issues
-			if(empty($requestObject->shiftUserId) === true) {
-				throw(new \InvalidArgumentException ("Shift user id cannot be empty", 405));
-			}
-			if(empty($requestObject->shiftCrewId) === true) {
-				throw(new \InvalidArgumentException ("Shift crew cannot be empty", 405));
-			}
-			if(empty($requestObject->shiftRequestId) === true) {
-				throw(new \InvalidArgumentException ("Shift request id cannot be empty", 405));
-			}
-			if(empty($requestObject->shiftStartTime) === true) {
-				throw(new \InvalidArgumentException ("Shift start time cannot be empty", 405));
-			}
-			if(empty($requestObject->shiftDuration) === true) {
-				throw(new \InvalidArgumentException ("Shift duration cannot be empty", 405));
-			}
-			if(empty($requestObject->shiftDate) === true) {
-				throw(new \InvalidArgumentException ("Shift date cannot be empty", 405));
-			}
-			if(empty($requestObject->shiftDelete) === true) {
-				$requestObject->shiftDelete = null;
-			}
+					//make sure all fields are present, in order to prevent database issues
+					if(empty($requestObject->shiftUserId) === true) {
+						throw(new \InvalidArgumentException ("Shift user id cannot be empty", 405));
+					}
+					if(empty($requestObject->shiftCrewId) === true) {
+						throw(new \InvalidArgumentException ("Shift crew cannot be empty", 405));
+					}
+					if(empty($requestObject->shiftRequestId) === true) {
+						throw(new \InvalidArgumentException ("Shift request id cannot be empty", 405));
+					}
+					if(empty($requestObject->shiftStartTime) === true) {
+						throw(new \InvalidArgumentException ("Shift start time cannot be empty", 405));
+					}
+					if(empty($requestObject->shiftDuration) === true) {
+						throw(new \InvalidArgumentException ("Shift duration cannot be empty", 405));
+					}
+					if(empty($requestObject->shiftDate) === true) {
+						throw(new \InvalidArgumentException ("Shift date cannot be empty", 405));
+					}
+					if(empty($requestObject->shiftDelete) === true) {
+						$requestObject->shiftDelete = null;
+					}
 
 
-			//perform the actual put or post
-			if($method === "PUT") {
-				$shift = Shift::getShiftByShiftId($pdo, $id);
-				if($shift === null) {
-					throw(new RuntimeException("Shift does not exist", 404));
+					//perform the actual put or post
+					if($method === "PUT") {
+						$shift = Shift::getShiftByShiftId($pdo, $id);
+						if($shift === null) {
+							throw(new RuntimeException("Shift does not exist", 404));
+						}
+
+						$shift = new Shift($id, $requestObject->shiftUserId, $requestObject->shiftCrewId, $requestObject->shiftRequestId, $requestObject->shiftStartTime, $requestObject->shiftDuration, $requestObject->shiftDate, $requestObject->shiftDelete);
+						$shift->update($pdo);
+
+						$reply->message = "Shift updated OK";
+
+					} elseif($method === "POST") {
+						$shift = new Shift($shiftId, $requestObject->shiftUserId, $requestObject->shiftCrewId, $requestObject->shiftRequestId, $requestObject->shiftStartTime, $requestObject->shiftDuration, $requestObject->shiftDate, $requestObject->shiftDelete);
+						$shift->insert($pdo);
+
+						$reply->message = "Shift created OK";
+					}
+
+				} else if($method === "DELETE") {
+					verifyXsrf();
+
+					$shift = Shift::getShiftByShiftId($pdo, $id);
+					if($shift === null) {
+						throw(new RuntimeException("Shift does not exist", 404));
+					}
+
+					$shift->delete($pdo);
+					$deleteObject = new \stdClass();
 				}
+				$deleteObject->shiftId = $id;
 
-				$shift = new Shift($id, $requestObject->shiftUserId, $requestObject->shiftCrewId, $requestObject->shiftRequestId, $requestObject->shiftStartTime, $requestObject->shiftDuration, $requestObject->shiftDate, $requestObject->shiftDelete);
-				$shift->update($pdo);
-
-				$reply->message = "Shift updated OK";
-
-			} elseif($method === "POST") {
-				$shift = new Shift($shiftId, $requestObject->shiftUserId, $requestObject->shiftCrewId, $requestObject->shiftRequestId, $requestObject->shiftStartTime, $requestObject->shiftDuration, $requestObject->shiftDate, $requestObject->shiftDelete);
-				$shift->insert($pdo);
-
-				$reply->message = "Shift created OK";
+				$reply->message = "Shift deleted OK";
 			}
-
-		} else if($method === "DELETE") {
-			verifyXsrf();
-
-			$shift = Shift::getShiftByShiftId($pdo, $id);
-			if($shift === null) {
-				throw(new RuntimeException("Shift does not exist", 404));
-			}
-
-			$shift->delete($pdo);
-			$deleteObject = new \stdClass();}
-			$deleteObject->ShiftId = $id;
-
-			$reply->message = "Shift deleted OK";
 		}
-	} else {
-		//if not an admin, and attempting a method other than get, throw an exception
-		if((empty($method) === false) && ($method !== "GET")) {
-			throw(new RuntimeException("Only administrators are allowed to modify entries", 401));
+		} else {
+			//if not an admin, and attempting a method other than get, throw an exception
+			if((empty($method) === false) && ($method !== "GET")) {
+				throw(new RuntimeException("Only administrators are allowed to modify entries", 401));
 			}
 		}
 
-	//send exception back to the caller
-} catch(Exception $exception) {
-	$reply->status = $exception->getCode();
-	$reply->message = $exception->getMessage();
-	$reply->trace = $exception->getTraceAsString();
-} catch(TypeError $typeError) {
-	$reply->status = $typeError->getCode();
-	$reply->message = $typeError->getMessage();
-}
+		//send exception back to the caller
+	} catch(Exception $exception) {
+		$reply->status = $exception->getCode();
+		$reply->message = $exception->getMessage();
+		$reply->trace = $exception->getTraceAsString();
+	} catch(TypeError $typeError) {
+		$reply->status = $typeError->getCode();
+		$reply->message = $typeError->getMessage();
+	}
 header("Content-type: application/json");
 if($reply->data === null) {
 	unset($reply->data);
