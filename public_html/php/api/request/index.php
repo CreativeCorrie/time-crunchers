9b0a8f5012fa13c99a7fb7e1a31d8907a4e2ed20
Sam Chandler <samuelvanchandler@gmail.com>
2016-03-01 09:55:21 -0700

    removed use User class

--- public_html/php/api/request/index.php
+++ public_html/php/api/request/index.php
@@ -13,7 +13,6 @@ require_once dirname(dirname(__DIR__)) . "/classes/autoloader.php";
 require_once dirname(dirname(__DIR__)) . "/lib/xsrf.php";
 require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
 use Edu\Cnm\Timecrunchers\Request;
-use Edu\Cnm\Timecrunchers\User;
 use Edu\Cnm\Timecrunchers\Access;
 
 
