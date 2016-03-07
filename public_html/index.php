ebf7f38379f3d3291534246a7772600d98f61d74
Elaine Thomas <enajera2@cnm.edu>
2016-03-06 20:10:41 -0700

    working on landing page with login modal, forgot password modal

--- public_html/index.php
+++ public_html/index.php
@@ -41,81 +41,176 @@
 	</head>
 
 	<title>Welcome to Time Crunch</title>
-	<body>
-		<header></header>
-		<div class="container">
-		</div>
-		<!-- form-->
+	<body id="landingPage">
+		<header id="landingHeader">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-8">
+						<h1>Welcome to Time Crunch</h1>
+					</div>
+				</div>
+			</div>
+		</header>
+
 		<div class="container">
 			<div class="row">
 				<div class="col-md-12">
-					<h1>Welcome to Time Crunch</h1>
 					<h2>Let's get started</h2>
 				</div>
 			</div>
 			<div class="row">
+
+				<!-- Account Holder drop-down with modals -->
 				<div class="col-md-5">
-					<h4>Already Have An Account?</h4>
+					<h4>Already Have an Account</h4>
 					<br>
-					<!-- Our Special dropdown has class show-on-hover -->
-					<div class="btn-group">
-						<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
-							I want to: <span class="caret"></span>
+					<!-- modal trigger buttons contained here -->
+					<div class="button-container">
+						<button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#modal-lg">Log In
 						</button>
-						<ul class="dropdown-menu" role="menu">
-							<li><a href="#">Log In</a></li>
-							<li><a href="#">Reset My Password</a></li>
-							<li class="divider"></li>
-							<li><a href="#">Get a Cupcake</a></li>
-						</ul>
+					</div>
+					<!-- ========================== -->
+					<!--  Large Modal               -->
+					<!-- ========================== -->
+					<div class="modal fade" id="modal-lg" tabindex="-1" role="dialog" aria-labelledby="largeModalLabel"
+						  aria-hidden="true" data-keyboard="true">
+						<!-- Modals have two optional sizes, available via class="modal-lg" or class="modal-sm" -->
+						<div class="modal-dialog modal-lg">
+
+							<!-- Begin modal content here -->
+							<div class="modal-content">
+
+								<div class="modal-header">
+									<!-- close button -->
+									<button type="button" class="close" data-dismiss="modal" aria-label="close">
+										<span aria-hidden="true">×</span>
+									</button>
+									<h3 class="modal-title">Take me to Time Crunch!</h3>
+								</div>
+
+								<div class="modal-body">
+									<!--									<p>Come to Time Crunch</p>-->
+									<label for="modalLoginForm" class="control-label">Enter your email address and password
+										here</label>
+									<form class="form-inline" id="modalLoginForm">
+
+										<div class="form-group">
+											<label for="emailLoginEmail" class="sr-only">Email: </label>
+											<div class="input-group">
+												<div class="input-group-addon"><span class="glyphicon glyphicon-envelope"></span>
+												</div>
+												<input type="email" class="form-control" id="emailLoginEmail"
+														 placeholder="enter email address"/>
+											</div>
+										</div>
+
+										<div class="form-group">
+											<label for="password" class="sr-only">Password: </label>
+											<div class="input-group">
+												<div class="input-group-addon"><span class="glyphicon glyphicon-plus-sign"></span>
+												</div>
+												<input type="text" class="form-control" id="password"
+														 placeholder="enter password"/>
+											</div>
+										</div>
+										<button type="submit" class="btn btn-info">Log In</button>
+									</form>
+								</div>
+							</div>
+						</div>
 					</div>
 				</div>
+				<!--end of Account Holder drop-downs & modals-->
+
+				<!-- empty div to separate buttons-->
 				<div class="col-md-2"></div>
+
+				<!-- New User drop-down button with modal -->
 				<div class="col-md-5">
 					<h4>New User?</h4>
 					<br>
-					<!-- Single button -->
-					<div class="btn-group">
-						<button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
-							Select: <span class="caret"></span>
+					<div class="dropdown">
+						<button type="button" class="btn btn-lg btn-warning dropdown-toggle" data-toggle="dropdown">Select:
+							<span class="caret"></span>
 						</button>
 						<ul class="dropdown-menu" role="menu">
-							<li><a href="#">I have my activation email</a></li>
+							<li><a href="#activate" data-toggle="modal">I have my activation email</a></li>
+							<!--placeholders -->
 							<li><a href="#"></a></li>
 							<li><a href="#"></a></li>
+							<li><a href="#"></a></li>
+
 							<li class="divider"></li>
 							<li><a href="#">I need to sign up my company/group for Time Crunch</a></li>
 						</ul>
+					</div>
+
+					<!--this is the Activate modal-->
+					<div class="modal fade" id="activate" data-target="#activate">
+						<div class="modal-dialog">
+							<div class="modal-content">
+								<div class="modal-header orange">
+									<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
+											aria-hidden="true">&times;</span>
+									</button>
+									<h4 class="modal-title"><strong></strong>Activation modal.</h4>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+			</div>
+		</div>
+
+		<div class="container">
+			<div class="row">
+				<div class="col=md-12">
+					<a href="#" data-target="#pwdModal" data-toggle="modal">Forgot my password</a>
+				</div>
+
+				<!--modal-->
+				<div id="pwdModal" class="modal fade" tabindex="-1" role="dialog" aria-hidden="true">
+					<div class="modal-dialog">
+						<div class="modal-content">
+							<div class="modal-header">
+								<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+								<h1 class="text-center">What's My Password?</h1>
+							</div>
+							<div class="modal-body">
+								<div class="col-md-12">
+									<div class="panel panel-default">
+										<div class="panel-body">
+											<div class="text-center">
+												<p>here.</p>
+												<div class="panel-body">
+													<fieldset>
+														<div class="form-group">
+															<input class="form-control input-lg" placeholder="E-mail Address"
+																	 name="email" type="email">
+														</div>
+														<input class="btn btn-lg btn-primary btn-block" value="Send My Password"
+																 type="submit">
+													</fieldset>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="modal-footer">
+								<div class="col-md-12">
+									<button class="btn" data-dismiss="modal" aria-hidden="true">Cancel</button>
+								</div>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
 		</div>
 
-		<!--form-->
-		<div class="dropdown">
-			<button type="button" class="btn btn-lg btn-warning dropdown-toggle" data-toggle="dropdown">By industries <span class="caret"></span>
-			</button>
-			<ul class="dropdown-menu" role="menu">
-				<li><a href="#activate" data-toggle="modal">I have my activation email</a></li>
-				<li><a href="#"></a></li>
-				<li><a href="#"></a></li>
-				<li><a href="#"></a></li>
-				<li class="divider"></li>
-				<li><a href="#">I need to sign up my company/group for Time Crunch</a></li>
-			</ul>
-		</div>
-		<div class="modal fade" id="activate" data-target="#activate">
-			<div class="modal-dialog">
-				<div class="modal-content">
-					<div class="modal-header orange">
-						<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span>
-						</button>
-						<!--this is the modal-->
-						<h4 class="modal-title"><strong></strong>Consumer goods</h4>
-					</div>
-				</div>
-			</div>
-		</div>
+
+		<!--end of drop down button with modals-->
 
 	</body>
 </html>
