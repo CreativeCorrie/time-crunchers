23d196154047defd1b1f0a49a2d419c8208fe5aa
Elaine Thomas <enajera2@cnm.edu>
2016-03-08 11:36:53 -0700

    adding activation token injection to newUserPass, still re-working landing page page buttons based on design flow

--- public_html/newUserSetPass.php
+++ public_html/newUserSetPass.php
@@ -7,6 +7,10 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
 		<meta name="viewport" content="width=device-width, initial-scale=1"/>
 
+		<!-- Font Awesome -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+
+
 		<!-- Latest compiled and minified CSS -->
 		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"
 				integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7"
@@ -42,6 +46,7 @@
 
 	<title></title>
 	<body>
+
 
 			<!-- Inject the activation token into the form -->
 		<?php
