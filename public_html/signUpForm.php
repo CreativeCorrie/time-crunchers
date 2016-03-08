876f3827c7d483502267251cc5a39b6d3a2796a6
Elaine Thomas <enajera2@cnm.edu>
2016-03-08 14:56:39 -0700

    minor changes to signUpForm field text

---
+++ public_html/signUpForm.php
@@ -0,0 +1,144 @@
+<div class="container">
+
+	<!-- Company Sign Up-->
+	<h2>First, sign up your Company or Group</h2>
+
+	<form>
+		<fieldset class="form-group">
+			<label for="formGroupExampleInput">Company Name</label>
+			<input type="text" class="form-control" id="companyName" placeholder="Findley's Tacos">
+		</fieldset>
+
+		<fieldset class="form-group">
+			<label for="formGroupExampleInput2">Attention Line</label>
+			<input type="text" class="form-control" id="companyAttn" placeholder="Tacos Findley, CEO">
+			<small class="text-muted">
+				This field is optional.
+			</small>
+		</fieldset>
+
+		<fieldset class="form-group">
+			<label for="formGroupExampleInput2">Company Address 1</label>
+			<input type="text" class="form-control" id="companyAddress1" placeholder="1600 Pennsylvania Ave NW">
+		</fieldset>
+
+		<fieldset class="form-group">
+			<label for="formGroupExampleInput2">Company Address 2</label>
+			<input type="text" class="form-control" id="companyAddress2" placeholder="Section 31">
+			<small class="text-muted">
+				This field is optional.
+			</small>
+		</fieldset>
+
+		<fieldset class="form-group">
+			<label for="formGroupExampleInput2">City</label>
+			<input type="text" class="form-control" id="companyCity" placeholder="Albuquerque">
+		</fieldset>
+
+		<fieldset class="form-group">
+			<label for="formGroupExampleInput2">State abbreviation</label>
+			<input type="text" class="form-control" id="companyState" placeholder="NM">
+		</fieldset>
+
+		<fieldset class="form-group">
+			<label for="formGroupExampleInput2">Zip Code</label>
+			<input type="text" class="form-control" id="companyZip" placeholder="87102">
+		</fieldset>
+
+		<fieldset class="form-group">
+			<label for="formGroupExampleInput2">Company URL</label>
+			<input type="text" class="form-control" id="companyUrl" placeholder="www.example.com">
+			<small class="text-muted">
+				This field is optional.
+			</small>
+		</fieldset>
+
+		<fieldset class="form-group">
+			<label for="formGroupExampleInput2">Company Email address</label>
+			<input type="text" class="form-control" id="companyEmail" placeholder="tacosFindley@wat.com">
+		</fieldset>
+	</form>
+
+	<br>
+	<hr>  <!-- Crew creation-->
+	<h2>Next, create your first crew.</h2>
+	<p class="text-danger">
+		This will be <em>your</em> crew and the crew for any additional administrators.
+	</p>
+
+	<form>
+		<fieldset class="form-group">
+			<label for="formGroupExampleInput2">Crew Location</label>
+			<input type="text" class="form-control" id="crewLocation" placeholder="Managers Only">
+			<small class="text-muted">
+				This name is how you will differentiate your crews.
+			</small>
+		</fieldset>
+	</form>
+
+	<!-- Admin sign up-->
+	<br>
+	<hr>
+	<h2>Ok, let's create <em>your</em> user profile</h2>
+	<p class="text-danger">
+		Somehow you will be their leader, you just need a cool name and admin access..
+	</p>
+
+	<form>
+		<fieldset class="form-group">
+			<label for="formGroupExampleInput2">Manager First Name</label>
+			<input type="text" class="form-control" id="userFirstName" placeholder="Suzy">
+		</fieldset>
+
+		<fieldset class="form-group">
+			<label for="formGroupExampleInput2">Manager Last Name</label>
+			<input type="text" class="form-control" id="userLastName" placeholder="Hughes">
+		</fieldset>
+
+		<fieldset class="form-group">
+			<label for="formGroupExampleInput2">Manager Phone Number</label>
+			<input type="text" class="form-control" id="userPhone" placeholder="505-555-1212">
+			<small class="text-muted">
+				This field is optional.
+			</small>
+		</fieldset>
+
+		<fieldset class="form-group">
+			<label for="formGroupExampleInput2">Manager Email Address</label>
+			<input type="text" class="form-control" id="userEmail" placeholder="suzyHughes@tacos.com">
+			<p class="text-danger">This is the email address your activation code will be sent to.</p>
+		</fieldset>
+	</form>
+
+	<!-- Create employee Invites-->
+	<br>
+	<hr>
+	<h2>Now you're ready to invite users to your Time Crunch Schedule</h2>
+	<p class="text-danger">Each user will be added to one of the crews you created above.</p>
+	<form>
+		<fieldset class="form-group">
+			<label for="formGroupExampleInput2">First Name</label>
+			<input type="text" class="form-control" id="userFirstName" placeholder="Talia">
+		</fieldset>
+
+		<fieldset class="form-group">
+			<label for="formGroupExampleInput2">Last Name</label>
+			<input type="text" class="form-control" id="userLastName" placeholder="Smith">
+		</fieldset>
+
+		<fieldset class="form-group">
+			<label for="formGroupExampleInput2">Phone Number</label>
+			<input type="text" class="form-control" id="userPhone" placeholder="505-555-1212">
+			<small class="text-muted">
+				This field is optional.
+			</small>
+		</fieldset>
+
+		<fieldset class="form-group">
+			<label for="formGroupExampleInput2">Employee Email Address</label>
+			<input type="text" class="form-control" id="userEmail" placeholder="talia@luna.com">
+			<p class="text-danger">This is the email address your activation code will be sent to.</p>
+		</fieldset>
+	</form>
+
+</div>
