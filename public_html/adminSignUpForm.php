546b0f3d7f5be3a0df45fc7cd8ca5ad5ef8d0208
Elaine Thomas <enajera2@cnm.edu>
2016-03-08 22:51:09 -0700

    made changes to text of email, changed sender name from Deep Dive Capstone to Time Crunch

--- public_html/adminSignUpForm.php
+++ public_html/adminSignUpForm.php
@@ -1,80 +1,84 @@
-	<!-- Company Sign Up-->
-	<h2>First, sign up your Company or Group</h2>
+<!-- Company Sign Up-->
+<!--		TODO: add Angualar.js here to connect to Company API-->
+<h2>First, sign up your Company or Group</h2>
 
-	<form>
-		<fieldset class="form-group">
-			<label for="companyNameInput">Company Name</label>
-			<input type="text" class="form-control" id="companyName" placeholder="Findley's Tacos">
-		</fieldset>
+<form>
+	<fieldset class="form-group">
+		<label for="companyNameInput">Company Name</label>
+		<input type="text" class="form-control" id="companyName" placeholder="Findley's Tacos">
+	</fieldset>
 
-		<fieldset class="form-group">
-			<label for="companyAttnInput">Attention Line</label>
-			<input type="text" class="form-control" id="companyAttn" placeholder="Tacos Findley, CEO">
-			<small class="text-muted">
-				This field is optional.
-			</small>
-		</fieldset>
+	<fieldset class="form-group">
+		<label for="companyAttnInput">Attention Line</label>
+		<input type="text" class="form-control" id="companyAttn" placeholder="Tacos Findley, CEO">
+		<small class="text-muted">
+			This field is optional.
+		</small>
+	</fieldset>
 
-		<fieldset class="form-group">
-			<label for="companyAddress1Input">Company Address 1</label>
-			<input type="text" class="form-control" id="companyAddress1" placeholder="1600 Pennsylvania Ave NW">
-		</fieldset>
+	<fieldset class="form-group">
+		<label for="companyAddress1Input">Company Address 1</label>
+		<input type="text" class="form-control" id="companyAddress1" placeholder="1600 Pennsylvania Ave NW">
+	</fieldset>
 
-		<fieldset class="form-group">
-			<label for="companyAddress2Input">Company Address 2</label>
-			<input type="text" class="form-control" id="companyAddress2" placeholder="Section 31">
-			<small class="text-muted">
-				This field is optional.
-			</small>
-		</fieldset>
+	<fieldset class="form-group">
+		<label for="companyAddress2Input">Company Address 2</label>
+		<input type="text" class="form-control" id="companyAddress2" placeholder="Section 31">
+		<small class="text-muted">
+			This field is optional.
+		</small>
+	</fieldset>
 
-		<fieldset class="form-group">
-			<label for="companyCityInput">City</label>
-			<input type="text" class="form-control" id="companyCity" placeholder="Albuquerque">
-		</fieldset>
+	<fieldset class="form-group">
+		<label for="companyCityInput">City</label>
+		<input type="text" class="form-control" id="companyCity" placeholder="Albuquerque">
+	</fieldset>
 
-		<fieldset class="form-group">
-			<label for="companyStateInput">State abbreviation</label>
-			<input type="text" class="form-control" id="companyState" placeholder="NM">
-		</fieldset>
+	<fieldset class="form-group">
+		<label for="companyStateInput">State abbreviation</label>
+		<input type="text" class="form-control" id="companyState" placeholder="NM">
+	</fieldset>
 
-		<fieldset class="form-group">
-			<label for="companyZipInput">Zip Code</label>
-			<input type="text" class="form-control" id="companyZip" placeholder="87102">
-		</fieldset>
+	<fieldset class="form-group">
+		<label for="companyZipInput">Zip Code</label>
+		<input type="text" class="form-control" id="companyZip" placeholder="87102">
+	</fieldset>
 
-		<fieldset class="form-group">
-			<label for="companyUrlInput">Company URL</label>
-			<input type="text" class="form-control" id="companyUrl" placeholder="www.example.com">
-			<small class="text-muted">
-				This field is optional.
-			</small>
-		</fieldset>
+	<fieldset class="form-group">
+		<label for="companyUrlInput">Company URL</label>
+		<input type="text" class="form-control" id="companyUrl" placeholder="www.example.com">
+		<small class="text-muted">
+			This field is optional.
+		</small>
+	</fieldset>
 
-		<fieldset class="form-group">
-			<label for="companyEmailInput">Company Email address</label>
-			<input type="text" class="form-control" id="companyEmail" placeholder="tacosFindley@wat.com">
-		</fieldset>
+	<fieldset class="form-group">
+		<label for="companyEmailInput">Company Email address</label>
+		<input type="text" class="form-control" id="companyEmail" placeholder="tacosFindley@wat.com">
+	</fieldset>
 
 
 	<br>
-	<hr>  <!-- Crew creation-->
+	<hr>
+	<!-- Crew creation-->
+	<!--		TODO: add Angualar.js here to connect to Crew API-->
 	<h2>Next, create your first crew.</h2>
 	<p class="text-danger">
 		This will be <em>your</em> crew and the crew for any additional administrators.
 	</p>
 
 
-		<fieldset class="form-group">
-			<label for="crewLocationInput">Crew Location</label>
-			<input type="text" class="form-control" id="crewLocation" placeholder="Managers Only">
-			<small class="text-muted">
-				This name is how you will differentiate your crews.
-			</small>
-		</fieldset>
+	<fieldset class="form-group">
+		<label for="crewLocationInput">Crew Location</label>
+		<input type="text" class="form-control" id="crewLocation" placeholder="Managers Only">
+		<small class="text-muted">
+			This name is how you will differentiate your crews.
+		</small>
+	</fieldset>
 
 
-	<!-- Admin sign up-->
+	<!-- Admin sign up = user sign up-->
+	<!--		TODO: add Angualar.js here to connect to User API-->
 	<br>
 	<hr>
 	<h2>Ok, let's create <em>your</em> user profile</h2>
@@ -82,49 +86,51 @@
 		Somehow you will be their leader, you just need a cool name and admin access..
 	</p>
 
-		<fieldset class="form-group">
-			<label for="userFirstNameInput">Manager First Name</label>
-			<input type="text" class="form-control" id="userFirstName" placeholder="Suzy">
-		</fieldset>
+	<fieldset class="form-group">
+		<label for="userFirstNameInput">Manager First Name</label>
+		<input type="text" class="form-control" id="userFirstName" placeholder="Suzy">
+	</fieldset>
 
-		<fieldset class="form-group">
-			<label for="userLastNameInput">Manager Last Name</label>
-			<input type="text" class="form-control" id="userLastName" placeholder="Hughes">
-		</fieldset>
+	<fieldset class="form-group">
+		<label for="userLastNameInput">Manager Last Name</label>
+		<input type="text" class="form-control" id="userLastName" placeholder="Hughes">
+	</fieldset>
 
-		<fieldset class="form-group">
-			<label for="userPhoneInput">Manager Phone Number</label>
-			<input type="text" class="form-control" id="userPhone" placeholder="505-555-1212">
-			<small class="text-muted">
-				This field is optional.
-			</small>
-		</fieldset>
+	<fieldset class="form-group">
+		<label for="userPhoneInput">Manager Phone Number</label>
+		<input type="text" class="form-control" id="userPhone" placeholder="505-555-1212">
+		<small class="text-muted">
+			This field is optional.
+		</small>
+	</fieldset>
 
-		<fieldset class="form-group">
-			<label for="userEmailInput">Manager Email Address</label>
-			<input type="text" class="form-control" id="userEmail" placeholder="suzyHughes@tacos.com">
-			<p class="text-danger">This is the email address your activation code will be sent to.</p>
-		</fieldset>
+	<fieldset class="form-group">
+		<label for="userEmailInput">Manager Email Address</label>
+		<input type="text" class="form-control" id="userEmail" placeholder="suzyHughes@tacos.com">
+		<p class="text-danger">This is the email address your activation code will be sent to.</p>
+	</fieldset>
 
-		<!--is user admin? code from http://www.bootply.com/0WvI1g4DEq-->
-		<h3>Will this user be an administrator?</h3>
-		<div class="input-group">
-			<div class="btn-group" data-toggle="buttons">
-				<label class="btn btn-default">
-					<input checked="checked" name="options" id="option1" type="radio"> Yes
-				</label>
-				<label class="btn btn-default">
-					<input name="options" id="option2" type="radio"> No
-				</label>
-			</div>
+	<!--is user admin? code from http://www.bootply.com/0WvI1g4DEq-->
+	<h3>Will this user be an administrator?</h3>
+	<div class="input-group">
+		<div class="btn-group" data-toggle="buttons">
+			<label class="btn btn-default">
+				<input checked="checked" name="options" id="option1" type="radio"> Yes
+			</label>
+			<label class="btn btn-default">
+				<input name="options" id="option2" type="radio"> No
+			</label>
 		</div>
-		<br>
-		<hr>
-		<br>
+	</div>
+	<br>
+	<hr>
+	<br>
 
-		<!-- Submit Form or Reset Form -->
-		<p>Great!  When you submit this form you will receive an email from "Deep Dive Capstone" with an activation link to reset your password.</p>
-		<button class="btn btn-success" type="submit"><i class="fa fa-paper-plane"></i> Submit</button>
-		<button class="btn btn-warning" type="reset"><i class="fa fa-ban"></i> Reset Form</button>
+	<!-- Submit Form or Reset Form -->
+	<!--		TODO: add Angualar.js here to connect to User API-->
+	<p>Great! When you submit this form you will receive an email from "Deep Dive Capstone" with an activation link to
+		reset your password.</p>
+	<button class="btn btn-success" type="submit"><i class="fa fa-paper-plane"></i> Submit</button>
+	<button class="btn btn-warning" type="reset"><i class="fa fa-ban"></i> Reset Form</button>
 
-	</form>
+</form>
--- public_html/newUserSetPass.php
+++ public_html/newUserSetPass.php
@@ -60,9 +60,11 @@ setXsrfCookie();
 		}
 		?>
 
+		<!--		TODO: add Angualar.js here to connect to User API-->
+
 		<div class="container">
 			<div class="row" ng-controller="ActivationController">
-				<div class="col-sm-6 col-sm-offset-3">
+				<div class="col-sm-8 col-sm-offset-3">
 					<h2>Choose a Password</h2>
 					<p class="text-center">Use the form below to set your password. Your password cannot be the same as your
 						username.</p>
@@ -72,26 +74,26 @@ setXsrfCookie();
 						<input type="password" class="input-lg form-control" name="password1" id="password1"
 								 placeholder="New Password" autocomplete="off" ng-model="activationData.password" ng-minlength="8" ng-required="true">
 						<div class="row">
-							<div class="col-sm-6">
-								<span id="8char" class="glyphicon glyphicon-remove setPass"></span> 8 Characters
+							<div class="col-sm-8">
+								<span id="8char" class="glyphicon glyphicon-flag setPass"></span> 8 Characters
 								Long<br>
-								<span id="ucase" class="glyphicon glyphicon-remove setPass"></span> One Uppercase
+								<span id="ucase" class="glyphicon glyphicon-flag setPass"></span> One Uppercase
 								Letter
 							</div>
-							<div class="col-sm-6">
-								<span id="lcase" class="glyphicon glyphicon-remove setPass"></span> One Lowercase
-								Letter<br>
-								<span id="num" class="glyphicon glyphicon-remove setPass"></span> One Number
+							<div class="col-sm-8">
+								<span id="num" class="glyphicon glyphicon-flag setPass"></span> One Number <br>
+								<span id="lcase" class="glyphicon glyphicon-flag setPass"></span> One Lowercase
+								Letter
 							</div>
 						</div>
 						<input type="password" class="input-lg form-control" name="confirmPassword" id="confirmPassword"
 								 placeholder="Repeat Password" autocomplete="off" ng-model="activationData.confirmPassword" ng-minlength="8" ng-required="true">
-						<div class="row">
-							<div class="col-sm-12">
-								<span id="pwmatch" class="glyphicon glyphicon-remove setPass"></span> Passwords
-								Match
-							</div>
-						</div>
+<!--						<div class="row">-->
+<!--							<div class="col-sm-12">-->
+<!--								<span id="pwmatch" class="glyphicon glyphicon-remove setPass"></span> Passwords-->
+<!--								Match-->
+<!--							</div>-->
+<!--						</div>-->
 						<input type="submit" class="col-xs-12 btn btn-primary btn-load btn-lg"
 								 data-loading-text="Changing Password..." value="Change Password">
 					</form>
