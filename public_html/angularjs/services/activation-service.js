58c795c7a9ccda17ee13f4e4acc6208ca1f0d361
Sam Chandler <samuelvanchandler@gmail.com>
2016-03-09 14:25:25 -0700

    cleaning up scheduleView nav bar

--- public_html/angularjs/services/activation-service.js
+++ public_html/angularjs/services/activation-service.js
@@ -3,7 +3,6 @@ app.service("ActivationService", function($http, ACTIVATION_ENDPOINT) {
 	function getUrl() {
 		return(ACTIVATION_ENDPOINT);
 	}
-
 	this.create = function(activation) {
 		return($http.post(getUrl(), activation));
 	};
--- public_html/scheduleView.php
+++ public_html/scheduleView.php
@@ -42,10 +42,7 @@
 	<title>Time Crunch Schedule View</title>
 	<body>
 		<header>
-
 			<!--beginning of nav bar-->
-
-
 			<!-- Wrap all page content here -->
 			<div id="wrap">
 				<nav class="navbar navbar-default" role="navigation">
@@ -58,78 +55,41 @@
 							<span class="icon-bar"></span>
 							<span class="icon-bar"></span>
 						</button>
-						<a class="navbar-brand" href="#">Test Nav bar 1</a>
+						<a class="navbar-brand" href="#">
+							<h2>Time Crunch</h2>
+						</a>
 					</div>
-
-					<!-- Collect the nav links, forms, and other content for toggling -->
-					<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-						<ul class="nav navbar-nav">
-							<li class="active"><a href="#">Link</a></li>
-							<li><a href="#">Link</a></li>
-							<li class="dropdown">
-								<a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
-								<ul class="dropdown-menu">
-									<li><a href="#">Action</a></li>
-									<li><a href="#">Another action</a></li>
-									<li><a href="#">Something else here</a></li>
-									<li class="divider"></li>
-									<li><a href="#">Separated link</a></li>
-									<li class="divider"></li>
-									<li><a href="#">One more separated link</a></li>
-								</ul>
-							</li>
-						</ul>
-
-						<!--this is where the sign in and activate buttons are -->
-						<form class="navbar-form navbar-left" role="search">
-							<!--<div class="form-group">-->
-							<!--<input type="text" class="form-control" placeholder="Search">-->
-							<!--</div>-->
-							<div class="button-container">
-
-								<!--Collapsible buttons-->
-								<ul class="nav navbar-nav navbar-right">
-
-									<li>
-										<button type="button" class="btn btn-warning navbar-btn" id="activate"
-												  data-toggle="modal"
-												  data-target="#modal-lg"><span
-												class="glyphicon glyphicon-check"></span> Admin
-										</button>
-									</li>
+					<!--this is where the sign in and activate buttons are -->
+					<form class="navbar-form navbar-right" role="search">
+						<div class="button-container">
+							<!--Collapsible buttons-->
+							<ul class="nav navbar-nav navbar-right">
+								<li>
+									<button type="button" class="btn btn-primary navbar-btn" id="#buttonSpacer">
+										<span class="glyphicon glyphicon-user"></span> Profile
+									</button>
+								</li>
+								<li>
+									<button type="button" class="btn btn-success navbar-btn" id="#buttonSpacer">
+									<span class="glyphicon glyphicon-cog"></span> Admin
+									</button>
+								</li>
+								<li>
+									<button type="button" class="btn btn-info navbar-btn" id="#buttonSpacer">
+										<span class="glyphyicon glyphicon-pencil"></span> Request
+									</button>
+								</li>
+								<li>
 									<!--TODO: Need angular here to end session and log user out-->
-									<li>
-										<button type="button" class="btn btn-danger navbar-btn"><span
-												class="glyphicon glyphicon-remove"></span> Log Out
-										</button>
-									</li>
-									<li>
-										<button type="button" class="btn btn-success navbar-btn id=" signIn
-										" data-toggle="modal"
-										data-target="#modal-lg""><span
-											class="glyphicon glyphicon-ok"></span> Activate
-										</button>
-									</li>
-								</ul>
-							</div>
-						</form>
-						<ul class="nav navbar-nav navbar-right">
-							<li><a href="#">Link</a></li>
-							<li class="dropdown">
-								<a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
-								<ul class="dropdown-menu">
-									<li><a href="#">Action</a></li>
-									<li><a href="#">Another action</a></li>
-									<li><a href="#">Something else here</a></li>
-									<li class="divider"></li>
-									<li><a href="#">Separated link</a></li>
-								</ul>
-							</li>
-						</ul>
-					</div><!-- /.navbar-collapse -->
-
+								<li>
+									<button type="button" class="btn btn-danger navbar-btn" id="#buttonSpacer">
+										<span class="glyphicon glyphicon-remove"></span> Log Out
+									</button>
+								</li>
+							</ul>
+						</div>
+					</form>
 				</nav>
-
 		</header>
 		<!-- Begin page content -->
 		<!--		main box-->
