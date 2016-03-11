7b0ec05b0e5c02c209b0d6b58768fe9f0f6f347e
Victoria Hooker <vhooker@cnm.edu>
2016-03-11 14:18:30 -0700

    did the crew controller.
    shift\index.php
    https://app.asana.com/0/84113659891169/92207056979008

--- public_html/angularjs/controllers/crew-controller.js
+++ public_html/angularjs/controllers/crew-controller.js
@@ -1,6 +1,39 @@
 app.controller('crewController', function($scope) {
 	$scope.alerts = [];
-	$scope.crewData = {};
+	$scope.crewData = [];
+	$scope.editedCrew = {};
+
+
+	//TODO: get data from form
+
+
+	/**
+	 * START METHOD(S): FETCH/GET
+	 *
+	 */
+
+	$scope.getCrewById = function() {
+		crewService.fetchCrewById()
+			.then(function(result) {
+				if(result.data.status === 200) {
+					$scope.crewData = result.data.data;  //TODO: is data.data correct.
+				} else {
+					$scope.alerts[0] = {type: "danger", msg: result.data.message};
+				}
+			})
+	};
+
+	$scope.getCrewByCompanyId = function() {
+		crewService.fetchCrewByCompanyId()
+			.then(function(result) {
+				if(result.data.status === 200) {
+					$scope.crewData = result.data.data;
+				} else {
+					$scope.alerts[0] = {type: "danger", msg: result.data.message};
+				}
+			})
+	};
+
 
 
 
