67fb000bec670ea72a27042e89a558e7e7084e36
Victoria Hooker <vhooker@cnm.edu>
2016-03-12 07:30:30 -0700

    made some small changes and corrected a spelling error in a doc block.
    shift\index.php
    https://app.asana.com/0/84113659891169/92207056979008

--- public_html/angularjs/controllers/login-controller.js
+++ public_html/angularjs/controllers/login-controller.js
@@ -1,3 +1,24 @@
-app.controller('loginController', function($scope) {
+app.controller("loginController", ["$scope", "loginService", function($scope, loginService) {
+	$scope.alerts = [];
+	$scope.loginData = {};
 
-});
+	/**
+	 * Method that uses the login service to login to an account
+	 *
+	 * @param loginData will contain login email and password
+	 * @param validated true if form is valid, false if not
+	 */
+
+	$scope.sendLogin = function(loginData, validated) {
+		if(validated === true) {
+			loginService.create(loginData)
+				.then(function(result) {
+					if(result.data.status === 200) {
+						$scope.alerts[0] = {type: "success", msg: result.data.message};
+					} else {
+						$scope.alerts[0] = {type: "danger", msg: result.data.message};
+					}
+				});
+		}
+	};
+}]);
--- public_html/angularjs/controllers/signup-controller.js
+++ public_html/angularjs/controllers/signup-controller.js
@@ -1,3 +1,24 @@
-app.controller('signupController', function($scope) {
+app.controller("ActivationController", ["$scope", "ActivationService", function($scope, ActivationService) {
+	$scope.alerts = [];
+	$scope.activationData = {};
 
-});
+	/**
+	 * Method that uses the activation service to activate an account
+	 *
+	 * @param activationData will contain activation token and password
+	 * @param validated true if form is valid, false if not
+	 */
+
+	$scope.sendActivation = function(activationData, validated) {
+		if(validated === true) {
+			ActivationService.create(activationData)
+				.then(function(result) {
+					if(result.data.status === 200) {
+						$scope.alerts[0] = {type: "success", msg: result.data.message};
+					} else {
+						$scope.alerts[0] = {type: "danger", msg: result.data.message};
+					}
+				});
+		}
+	};
+}]);
