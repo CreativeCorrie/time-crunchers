1ac2bdc3db3c481bffa6053cfd2158f437e90462
Elaine Thomas <enajera2@cnm.edu>
2016-03-14 14:28:42 -0600

    added a temp link on landing oage for add schedule form

--- public_html/angularjs/pages/landingPage.php
+++ public_html/angularjs/pages/landingPage.php
@@ -36,15 +36,18 @@
 						<!--									<p>Come to Time Crunch LOL</p>-->
 						<label for="modalLoginForm" class="control-label">Enter your email address and password
 							here</label>
-						<form class="form-inline" id="modalLoginForm" name="modalLoginForm" ng-submit="sendLogin(loginData, loginData.$valid);">
+						<form class="form-inline" id="modalLoginForm" name="modalLoginForm"
+								ng-submit="sendLogin(loginData, loginData.$valid);">
 							<div class="form-group">
 								<label for="emailLoginEmail" class="sr-only">Email: </label>
 								<div class="input-group">
 									<div class="input-group-addon"><span class="glyphicon glyphicon-envelope"></span>
 									</div>
 									<input type="email" class="form-control" id="loginEmail" name="loginEmail"
-											 placeholder="enter email address" ng-model="loginData.userEmail" ng-minlength="6" ng-maxlength="64" ng-required="true"/>
-									<div class="alert alert-danger" role="alert" ng-messages="modalLoginForm.loginEmail.$error" ng-if="modalLoginForm.loginEmail.$touched" ng-hide="modalLoginForm.loginEmail.$valid">
+											 placeholder="enter email address" ng-model="loginData.userEmail" ng-minlength="6"
+											 ng-maxlength="64" ng-required="true"/>
+									<div class="alert alert-danger" role="alert" ng-messages="modalLoginForm.loginEmail.$error"
+										  ng-if="modalLoginForm.loginEmail.$touched" ng-hide="modalLoginForm.loginEmail.$valid">
 										<p ng-message="minlength">Email is too short.</p>
 										<p ng-message="maxlength">Email is too long.</p>
 										<p ng-message="required">Please enter your email.</p>
@@ -57,8 +60,11 @@
 									<div class="input-group-addon"><span class="glyphicon glyphicon-plus-sign"></span>
 									</div>
 									<input type="password" class="form-control" id="userPassword" name="userPassword"
-											 placeholder="enter password" ng-model="loginData.userPassword" ng-minlength="8" ng-required="true"/>
-									<div class="alert alert-danger" role="alert" ng-messages="modalLoginForm.userPassword.$error" ng-if="modalLoginForm.userPassword.$touched" ng-hide="modalLoginForm.userPassword.$valid">
+											 placeholder="enter password" ng-model="loginData.userPassword" ng-minlength="8"
+											 ng-required="true"/>
+									<div class="alert alert-danger" role="alert" ng-messages="modalLoginForm.userPassword.$error"
+										  ng-if="modalLoginForm.userPassword.$touched"
+										  ng-hide="modalLoginForm.userPassword.$valid">
 										<p ng-message="minlength">Password is too short.</p>
 										<p ng-message="required">Please enter your password.</p>
 									</div>
@@ -70,14 +76,20 @@
 				</div>
 			</div>
 		</div> <!-- End of Log in modal-->
-		<div class="col-md-12"></div>
+
+		<div class="col-md-5"></div>
+		<div class="container">
+			<a id="buttonSpacer" href="public_html/angularjs/templates/adminSignUpForm.php">Sign Up</a>
+		</div>
 		<div class="row">
-			<!-- sign up -->
-			<div class="container">
-				<a id="buttonSpacer" href="../templates/adminSignUpForm.php">Sign Up</a>
-				<!-- forgot password-->
-				<a href="setPassForm.php">Forgot my password</a>
-			</div>
+		<div class="col-md-12"></div>
+				<!-- sign up -->
+
+			<!-- temp addschedule button -->
+
+			<a href="public_html/angularjs/templates/addScheduleForm.php"> Temp Link to Add Schedule form</a>
+
+
 		</div>
 	</div>
 	<!--end of Account Holder content-->
