cd889e5db1e384404d8fefe8bd159e05fce14e11
DenzylFontaine <ddenzfong@gmail.com>
2016-03-13 19:54:22 -0600

    put the request form in a div for rows and columns but i realize now that a container is needed

--- public_html/angularjs/templates/requestModal.php
+++ public_html/angularjs/templates/requestModal.php
@@ -1,55 +1,60 @@
 <!-- this form is for an employee that needs to request time off. -->
 
-<form>
+<h2>Make your Request</h2>
 
-	<fieldset class="form-group">
-		<label for="userFirstNameInput">Employee First Name</label>
-		<input type="text" class="form-control" id="userFirstName" placeholder="Talia">
+<div class="row">
+	<div class="col-md-8">
+		<form>
 
-		<label for="userLastNameInput">Employee Last Name</label>
-		<input type="text" class="form-control" id="userLastName" placeholder="Martinez">
-	</fieldset>
+			<fieldset class="form-group">
+				<label for="userFirstNameInput">Employee First Name</label>
+				<input type="text" class="form-control" id="userFirstName" placeholder="Talia">
 
-	<fieldset class="form-group">
-		<label for="userPhoneInput">Employee Phone Number</label>
-		<input type="text" class="form-control" id="userPhone" placeholder="999-888-7777">
-		<small class="text-muted">
-			This field is optional.
-		</small>
-	</fieldset>
+				<label for="userLastNameInput">Employee Last Name</label>
+				<input type="text" class="form-control" id="userLastName" placeholder="Martinez">
+			</fieldset>
 
-	<fieldset class="form-group">
-		<label for="userEmailInput">Employee Email Address</label>
-		<input type="text" class="form-control" id="userEmail" placeholder="taliamartinez@tacos.com">
-		<p class="text-danger">This is the email address your activation code will be sent to.</p>
-	</fieldset>
+			<fieldset class="form-group">
+				<label for="userPhoneInput">Employee Phone Number</label>
+				<input type="text" class="form-control" id="userPhone" placeholder="999-888-7777">
+				<small class="text-muted">
+					This field is optional.
+				</small>
+			</fieldset>
 
-	<fieldset class="form-group">
-		<label for="crewLocationInput">Crew Location</label>
-		<input type="text" class="form-control" id="crewLocation" placeholder="Managers Only">
-		<small class="text-muted">
-			This name is how you will differentiate your crews.
-		</small>
-	</fieldset>
+			<fieldset class="form-group">
+				<label for="userEmailInput">Employee Email Address</label>
+				<input type="text" class="form-control" id="userEmail" placeholder="taliamartinez@tacos.com">
+				<p class="text-danger">This is the email address your activation code will be sent to.</p>
+			</fieldset>
 
-	<hr>
+			<fieldset class="form-group">
+				<label for="crewLocationInput">Crew Location</label>
+				<input type="text" class="form-control" id="crewLocation" placeholder="Managers Only">
+				<label for="companyNameInput">Company Name</label>
+				<input type="text" class="form-control" id="companyName" placeholder="Findley's Tacos">
+			</fieldset>
 
-	<legend>Choose type of Request</legend>
-	<p>
-		<select id="myList">
-			<option value 0>select the type of Request you would like to make</option>
-			<option value 1>Vacation Time</option>
-			<option value 2>Sick/Maturnity Leave</option>
-			<option value 3>Emergency</option>
-			<option value 4>Transfer</option>
-		</select>
-	</p>
+			<hr>
 
-	<fieldset class="form-group">
-		<label for="userRequest">Leave your Request</label>
-		</br>
-		<textarea class="form-control" rows="8" columns="20" id="userRequest" name="request" placeholder="To whom it may concern..."></textarea>
-	</fieldset>
+			<legend>Choose type of Request</legend>
+			<p>
+				<select id="myList">
+					<option value 0>select the type of Request you would like to make</option>
+					<option value 1>Vacation Time</option>
+					<option value 2>Sick/Maturnity Leave</option>
+					<option value 3>Emergency</option>
+					<option value 4>Transfer</option>
+				</select>
+			</p>
+
+			<fieldset class="form-group">
+				<label for="userRequest">Leave your Request</label>
+				</br>
+				<textarea class="form-control" rows="8" columns="20" id="userRequest" name="request" placeholder="To whom it may concern..."></textarea>
+			</fieldset>
 
 
-</form>
+		</form>
+	</div>
+</div>
