f4fc3f0f6eb30708c60ed7bc596a6ef506d686cc
Victoria Hooker <vhooker@cnm.edu>
2016-03-14 13:26:59 -0600

    Merge branch 'master' of C:\Users\Victoria\Desktop\Bootcamp\Git\time-crunchers with conflicts.

--- public_html/angularjs/controllers/company-controller.js
+++ public_html/angularjs/controllers/company-controller.js
@@ -1,3 +1,109 @@
 app.controller('CompanyController', function($scope) {
 
+	$scope.alerts = [];
+	$scope.companyData = [];
+	$scope.editedCompany = {};
+
+	/**
+	 * START METHOD(S): FETCH/GET
+	 *
+	 */
+
+	$scope.getScheduleById = function() {
+		scheduleService.fetchScheduleById(scheduleId)
+			.then(function(result) {
+				if(result.data.status === 200) {
+					$scope.scheduleData = result.data.data;
+				} else {
+					$scope.alerts[0] = {type: "danger", msg: result.data.message};
+				}
+			})
+	};
+
+	$scope.getScheduleByScheduleId = function() {
+		scheduleService.fetchScheduleByScheduleId(scheduleScheduleId)
+			.then(function(result) {
+				if(result.data.status === 200) {
+					$scope.scheduleData = result.data.data;
+				} else {
+					$scope.alerts[0] = {type: "danger", msg: result.data.message};
+				}
+			})
+	};
+
+	$scope.getAllSchedules = function() {
+		scheduleService.fetchAllSchedules()
+			.then(function(result) {
+				if(result.data.status === 200) {
+					$scope.scheduleData = result.data.data;
+				} else {
+					$scope.alerts[0] = {type: "danger", msg: result.data.message};
+				}
+			})
+	};
+
+	// subscript to the update channel; this will update the schedules array on demand
+	Pusher.subscribe("schedule", "update", function(schedule) {
+		for(var i = 0; i < $scope.schedules.length; i++) {
+			if($scope.schedules[i].scheduleId === schedule.scheduleId) {
+				$scope.schedules[i] = schedule;
+				break;
+			}
+		}
+	});
+
+
+	/**
+	 * creates a schedule and sends it to the schedule API
+	 *
+	 * @param schedule the schedule to send
+	 * @param validated true if Angular validated the form, false if not
+	 **/
+	$scope.createSchedule = function(schedule, validated) {
+		if(validated === true) {
+			ScheduleService.create(schedule)
+				.then(function(result) {
+					if(result.data.status === 200) {
+						$scope.alerts[0] = {type: "success", msg: result.data.message};
+						$scope.newSchedule = {scheduleId: null, attribution: "", schedule: "", submitter: ""};
+						$scope.addScheduleForm.$setPristine();
+						$scope.addScheduleForm.$setUntouched();
+					} else {
+						$scope.alerts[0] = {type: "danger", msg: result.data.message};
+					}
+				});
+		}
+	};
+
+	// subscribe to the delete channel; this will delete from the schedule array on demand
+	Pusher.subscribe("schedule", "delete", function(schedule) {
+		for(var i = 0; i < $scope.schedules.length; i++) {
+			if($scope.schedules[i].scheduleId === schedule.scheduleId) {
+				$scope.schedules.splice(i, 1);
+				break;
+			}
+		}
+	});
+
+	// embedded modal instance controller to create deletion prompt
+	var ModalInstanceCtrl = function($scope, $uibModalInstance) {
+		$scope.yes = function() {
+			$uibModalInstance.close();
+		};
+
+		$scope.no = function() {
+			$uibModalInstance.dismiss('cancel');
+		};
+	};
+
+
+
+
+
+
+
+
+
+
+
 });
--- public_html/angularjs/controllers/schedule-controller.js
+++ public_html/angularjs/controllers/schedule-controller.js
@@ -20,8 +20,8 @@ app.controller('ScheduleController', function($scope) {
 			})
 	};
 
-	$scope.getScheduleByCrewId = function() {
-		scheduleService.fetchScheduleByCrewId(scheduleCrewId)
+	$scope.getScheduleByScheduleId = function() {
+		scheduleService.fetchScheduleByScheduleId(scheduleScheduleId)
 			.then(function(result) {
 				if(result.data.status === 200) {
 					$scope.scheduleData = result.data.data;
@@ -41,5 +41,69 @@ app.controller('ScheduleController', function($scope) {
 				}
 			})
 	};
+
+	// subscript to the update channel; this will update the schedules array on demand
+	Pusher.subscribe("schedule", "update", function(schedule) {
+		for(var i = 0; i < $scope.schedules.length; i++) {
+			if($scope.schedules[i].scheduleId === schedule.scheduleId) {
+				$scope.schedules[i] = schedule;
+				break;
+			}
+		}
+	});
+
+
+	/**
+	 * creates a schedule and sends it to the schedule API
+	 *
+	 * @param schedule the schedule to send
+	 * @param validated true if Angular validated the form, false if not
+	 **/
+	$scope.createSchedule = function(schedule, validated) {
+		if(validated === true) {
+			ScheduleService.create(schedule)
+				.then(function(result) {
+					if(result.data.status === 200) {
+						$scope.alerts[0] = {type: "success", msg: result.data.message};
+						$scope.newSchedule = {scheduleId: null, attribution: "", schedule: "", submitter: ""};
+						$scope.addScheduleForm.$setPristine();
+						$scope.addScheduleForm.$setUntouched();
+					} else {
+						$scope.alerts[0] = {type: "danger", msg: result.data.message};
+					}
+				});
+		}
+	};
+
+	// subscribe to the delete channel; this will delete from the schedule array on demand
+	Pusher.subscribe("schedule", "delete", function(schedule) {
+		for(var i = 0; i < $scope.schedules.length; i++) {
+			if($scope.schedules[i].scheduleId === schedule.scheduleId) {
+				$scope.schedules.splice(i, 1);
+				break;
+			}
+		}
+	});
+
+	// embedded modal instance controller to create deletion prompt
+	var ModalInstanceCtrl = function($scope, $uibModalInstance) {
+		$scope.yes = function() {
+			$uibModalInstance.close();
+		};
+
+		$scope.no = function() {
+			$uibModalInstance.dismiss('cancel');
+		};
+	};
+
+
+
+
+
+
+
+
+
+
 
 });
