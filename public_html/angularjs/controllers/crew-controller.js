1853e5d781ca933e8ffe70b104592deb3d62a45a
Victoria Hooker <vhooker@cnm.edu>
2016-03-14 09:47:19 -0600

    Finshed the Shift and Crew controllers
    shift\index.php
    https://app.asana.com/0/84113659891169/92207056979008

--- public_html/angularjs/controllers/crew-controller.js
+++ public_html/angularjs/controllers/crew-controller.js
@@ -46,6 +46,60 @@ app.controller('CrewController', function($scope) {
 			})
 	};
 
+	// subscript to the update channel; this will update the crews array on demand
+	Pusher.subscribe("crew", "update", function(crew) {
+		for(var i = 0; i < $scope.crews.length; i++) {
+			if($scope.crews[i].crewId === shift.crewId) {
+				$scope.crews[i] = crew;
+				break;
+			}
+		}
+	});
+
+
+	/**
+	 * creates a crew and sends it to the crew API
+	 *
+	 * @param crew the crew to send
+	 * @param validated true if Angular validated the form, false if not
+	 **/
+	$scope.createCrew = function(crew, validated) {
+		if(validated === true) {
+			CrewService.create(crew)
+				.then(function(result) {
+					if(result.data.status === 200) {
+						$scope.alerts[0] = {type: "success", msg: result.data.message};
+						$scope.newShift = {shiftId: null, attribution: "", crew: "", submitter: ""};
+						$scope.addCrewForm.$setPristine();
+						$scope.addCrewForm.$setUntouched();
+					} else {
+						$scope.alerts[0] = {type: "danger", msg: result.data.message};
+					}
+				});
+		}
+	};
+
+	// subscribe to the delete channel; this will delete from the crew array on demand
+	Pusher.subscribe("crew", "delete", function(crew) {
+		for(var i = 0; i < $scope.crews.length; i++) {
+			if($scope.crews[i].crewId === shift.crewId) {
+				$scope.crews.splice(i, 1);
+				break;
+			}
+		}
+	});
+
+	// embedded modal instance controller to create deletion prompt
+	var ModalInstanceCtrl = function($scope, $uibModalInstance) {
+		$scope.yes = function() {
+			$uibModalInstance.close();
+		};
+
+		$scope.no = function() {
+			$uibModalInstance.dismiss('cancel');
+		};
+	};
+
 
 
 
--- public_html/angularjs/controllers/shift-controller.js
+++ public_html/angularjs/controllers/shift-controller.js
@@ -1,10 +1,31 @@
-app.controller('ShiftController', function($scope) {
+app.controller('CrewController', function($scope) {
 	$scope.alerts = [];
 	$scope.shiftData = [];
 	$scope.editedShift = {};
 
+
+	//TODO: get data from form
+
+
+	/**
+	 * START METHOD(S): FETCH/GET
+	 *
+	 */
+
 	$scope.getShiftById = function() {
-		shiftService.fetchShiftById(shiftId)
+		shiftService.fetchShiftById(shiftwId)
+			.then(function(result) {
+				if(result.data.status === 200) {
+					$scope.crewData = result.data.data;  //TODO: is data.data correct.
+
+				} else {
+					$scope.alerts[0] = {type: "danger", msg: result.data.message};
+				}
+			})
+	};
+
+	$scope.getshiftByShiftUserId = function() {
+		shiftService.fetchShiftByShiftUserId(shiftUserId)
 			.then(function(result) {
 				if(result.data.status === 200) {
 					$scope.shiftData = result.data.data;
@@ -14,25 +35,57 @@ app.controller('ShiftController', function($scope) {
 			})
 	};
 
-	$scope.getShiftById = function() {
-		shiftService.fetchShiftByUserId(shiftUserId)
-			.then(function(result) {
-				if(result.data.status === 200) {
-					$scope.shiftData = result.data.data;
-				} else {
-					$scope.alerts[0] = {type: "danger", msg: result.data.message};
-				}
-			})
+	// subscript to the update channel; this will update the shifts array on demand
+	Pusher.subscribe("shift", "update", function(shift) {
+		for(var i = 0; i < $scope.shifts.length; i++) {
+			if($scope.shifts[i].shiftId === shift.shiftId) {
+				$scope.shifts[i] = shift;
+				break;
+			}
+		}
+	});
+
+
+	/**
+	 * creates a shift and sends it to the shift API
+	 *
+	 * @param shift the shift to send
+	 * @param validated true if Angular validated the form, false if not
+	 **/
+	$scope.createShift = function(shift, validated) {
+		if(validated === true) {
+			ShiftService.create(shift)
+				.then(function(result) {
+					if(result.data.status === 200) {
+						$scope.alerts[0] = {type: "success", msg: result.data.message};
+						$scope.newShift = {shiftId: null, attribution: "", shift: "", submitter: ""};
+						$scope.addShiftForm.$setPristine();
+						$scope.addShiftForm.$setUntouched();
+					} else {
+						$scope.alerts[0] = {type: "danger", msg: result.data.message};
+					}
+				});
+		}
 	};
 
-	$scope.getAllShifts = function() {
-		shiftService.fetchAllShifts()
-			.then(function(result) {
-				if(result.data.status === 200) {
-					$scope.shiftData = result.data.data;
-				} else {
-					$scope.alerts[0] = {type: "danger", msg: result.data.message};
-				}
-			})
+	// subscribe to the delete channel; this will delete from the shift array on demand
+	Pusher.subscribe("shift", "delete", function(shift) {
+		for(var i = 0; i < $scope.shifts.length; i++) {
+			if($scope.shifts[i].shiftId === shift.shiftId) {
+				$scope.shifts.splice(i, 1);
+				break;
+			}
+		}
+	});
+
+	// embedded modal instance controller to create deletion prompt
+	var ModalInstanceCtrl = function($scope, $uibModalInstance) {
+		$scope.yes = function() {
+			$uibModalInstance.close();
+		};
+
+		$scope.no = function() {
+			$uibModalInstance.dismiss('cancel');
+		};
 	};
 });
