aad8ed30e837566e7050aa6c89d44db0ec3b9112
Sam Chandler <samuelvanchandler@gmail.com>
2016-03-14 13:38:25 -0600

    added delete and update to user controller

--- public_html/angularjs/controllers/request-controller.js
+++ public_html/angularjs/controllers/request-controller.js
@@ -1,13 +1,11 @@
-app.controller('RequestController', function($scope) {
+app.controller('RequestController', ["$scope","requestService", function($scope, requestService) {
 	$scope.alerts = [];
 	$scope.requestData = {};
-	$scope.editedRequest = {};
 
 	/**
 	 * START METHOD(S): FETCH/GET
 	 *
 	 */
-
 	$scope.getRequestById = function() {
 		requestService.fetchRequestById(requestId)
 			.then(function(result) {
@@ -31,7 +29,27 @@ app.controller('RequestController', function($scope) {
 	};
 
 	/**
-	 * updates a request and sends it to the misquote API
+	 * creates a request and sends it to the request API
+	 *
+	 * @param request the request to send
+	 * @param validated true if Angular validated the form, false if not
+	 **/
+	$scope.createRequest = function(request, validated) {
+		if(validated === true) {
+			requestService.create(request)
+				.then(function(result) {
+					if(result.data.status === 200) {
+						$scope.alerts[0] = {type: "success", msg: result.data.message};
+						$scope.newRequest = {requestId: null, requestRequestorId: null, requestAdminId: null, requestTimeStamp: "", requestActionTimeStamp: "", requestApprove: 0, requestRequestorText: "", requestAdminText: ""};
+					} else {
+						$scope.alerts[0] = {type: "danger", msg: result.data.message};
+					}
+				});
+		}
+	};
+
+	/**
+	 * updates a request and sends it to the request API
 	 *
 	 * @param request the request to send
 	 * @param validated true if Angular validated the form, false if not
@@ -48,50 +66,9 @@ app.controller('RequestController', function($scope) {
 				});
 		}
 	};
-
-	/**
-	 * deletes a request and sends it to the misquote API, if the user confirms deletion
-	 *
-	 * @param requestId the misquote id to delete
-	 **/
-	$scope.deleteRequest = function(requestId) {
-		// create a modal instance to prompt the user if she/he is sure they want to delete the misquote
-		var message = "Do you really want to delete this request?";
-
-		var modalHtml = '<div class="modal-body">' + message + '</div><div class="modal-footer"><button class="btn btn-primary" ng-click="yes()">Yes</button><button class="btn btn-warning" ng-click="no()">No</button></div>';
-
-		var modalInstance = $uibModal.open({
-			template: modalHtml,
-			controller: ModalInstanceCtrl
-		});
-
-		// if the user clicked "yes", delete the misquote
-		modalInstance.result.then(function () {
-			requestService.destroy(requestId)
-				.then(function(result) {
-					if(result.data.status === 200) {
-						$scope.alerts[0] = {type: "success", msg: result.data.message};
-						$scope.deletedRequest = true;
-					} else {
-						$scope.alerts[0] = {type: "danger", msg: result.data.message};
-					}
-				})
-		});
-	};
-
+// really not sure if this belongs
 	if($scope.request === null) {
 		$scope.getRequest();
 	}
 
-
-});
-
-var ModalInstanceCtrl = function($scope, $uibModalInstance) {
-	$scope.yes = function() {
-		$uibModalInstance.close();
-	};
-
-	$scope.no = function() {
-		$uibModalInstance.dismiss('cancel');
-	};
-};
+}]);
