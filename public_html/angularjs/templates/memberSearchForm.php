de31d94cec143343483af822a6613c78b461496b
Victoria Hooker <vhooker@cnm.edu>
2016-03-15 11:10:15 -0600

    Made corrections as discussed in memberSearchForm
    shift\index.php
    https://app.asana.com/0/84113659891169/92207056979008

--- public_html/angularjs/templates/memberSearchForm.php
+++ public_html/angularjs/templates/memberSearchForm.php
@@ -3,20 +3,44 @@
 	<h2>Search for Member</h2>
 
 	<p class="text-danger">Please enter Member's First <em>AND</em> Last Name.</p>
-	<form>
+	<form name="memberSearchForm" ng-submit="sendActivation(signupData, userSignUpForm.$valid);">
 		<fieldset class="form-group">
 			<label for="userFirstNameInput">Member First Name</label>
-			<input type="text" class="form-control" id="userFirstName" placeholder="Bob">
+			<input type="text" class="form-control" name="userFirstName" id="userFirstName"
+					 placeholder="Bob" ng-model="memberSearchForm.userFirstName"
+					 ng-minlength="2" ng-maxlength="128" ng-required="true"/>
+			<div class="alert alert-danger" role="alert" ng-messages="memberSearchForm.userFirstName.$error"
+				  ng-if="memberSearchForm.userFirstName.$touched" ng-hide="memberSearchForm.userFirstName.$valid">
+				<p ng-message="minlength">Name is too short.</p>
+				<p ng-message="maxlength">Name is too long.</p>
+				<p ng-message="required">Please enter your name.</p>
+			</div>
 		</fieldset>
 		<fieldset class="form-group">
 			<label for="userLastNameInput">Member Last Name</label>
-			<input type="text" class="form-control" id="userLastName" placeholder="Dobbs">
+			<input type="text" class="form-control" name="userLastName" id="userLastName"
+					 placeholder="Dobbs" ng-model="memberSearchForm.userLastName"
+					 ng-minlength="2" ng-maxlength="128" ng-required="true"/>
+			<div class="alert alert-danger" role="alert" ng-messages="memberSearchForm.userLastName.$error"
+				  ng-if="memberSearchForm.userLastName.$touched" ng-hide="memberSearchForm.userLastName.$valid">
+				<p ng-message="minlength">Name is too short.</p>
+				<p ng-message="maxlength">Name is too long.</p>
+				<p ng-message="required">Please enter your name.</p>
+			</div>
 		</fieldset>
 
 		<h3><em>Or</em></h3>
 		<fieldset class="form-group">  <!-- get user by email address -->
 			<label for="userEmailInput">Member Email Address</label>
-			<input type="text" class="form-control" id="userLastName" placeholder="bob@cosg.com">
+			<input type="text" class="form-control" name="userEmail" id="userEmail"
+					 placeholder="bob@cosg.com" ng-model="memberSearchForm.userEmail"
+					 ng-minlength="2" ng-maxlength="128" ng-required="true"/>
+			<div class="alert alert-danger" role="alert" ng-messages="memberSearchForm.userEmail.$error"
+				  ng-if="memberSearchForm.userEmail.$touched" ng-hide="memberSearchForm.userEmail.$valid">
+				<p ng-message="minlength">Email is too short.</p>
+				<p ng-message="maxlength">Email is too long.</p>
+				<p ng-message="required">Please enter your email.</p>
+			</div>
 		</fieldset>
 
 
