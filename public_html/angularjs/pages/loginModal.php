7ae93fd08c507bb40b13a400310d5a7a530b5967
Rochelle Lewis <rochelle.a.lewis@gmail.com>
2016-03-16 09:22:33 -0600

    sort out login modal HTML

--- public_html/angularjs/pages/loginModal.php
+++ public_html/angularjs/pages/loginModal.php
@@ -1,13 +1,18 @@
-<form class="form-inline" id="modalLoginForm" name="modalLoginForm" ng-submit="ok();" novalidate>
-	<div class="form-group">
+<div class="modal-header">
+	<h3>Log In</h3>
+</div>
+<div class="modal-body">
+	<form id="modalLoginForm" name="modalLoginForm" ng-submit="submit(login, modalLoginForm.$valid);" novalidate>
+		<div class="form-group">
 
-		<label for="emailLoginEmail" class="sr-only">Email: </label>
-		<div class="input-group">
-			<div class="input-group-addon"><span class="glyphicon glyphicon-envelope"></span>
+			<label for="emailLoginEmail" class="sr-only">Email: </label>
+			<div class="input-group">
+				<div class="input-group-addon"><span class="glyphicon glyphicon-envelope"></span>
+				</div>
+				<input type="email" class="form-control" id="loginEmail" name="loginEmail"
+						 placeholder="enter email address" ng-model="loginData.userEmail" ng-minlength="6"
+						 ng-maxlength="64" ng-required="true"/>
 			</div>
-			<input type="email" class="form-control" id="loginEmail" name="loginEmail"
-					 placeholder="enter email address" ng-model="loginData.userEmail" ng-minlength="6"
-					 ng-maxlength="64" ng-required="true"/>
 			<div class="alert alert-danger" role="alert" ng-messages="modalLoginForm.loginEmail.$error"
 				  ng-if="modalLoginForm.loginEmail.$touched" ng-hide="modalLoginForm.loginEmail.$valid">
 				<p ng-message="minlength">Email is too short.</p>
@@ -15,23 +20,23 @@
 				<p ng-message="required">Please enter your email.</p>
 			</div>
 		</div>
-	</div>
-	<div class="form-group">
-
-		<label for="password" class="sr-only">Password: </label>
-		<div class="input-group">
-			<div class="input-group-addon"><span class="glyphicon glyphicon-plus-sign"></span>
+		<div class="form-group">
+			<label for="password" class="sr-only">Password: </label>
+			<div class="input-group">
+				<div class="input-group-addon">
+					<span class="glyphicon glyphicon-plus-sign"></span>
+				</div>
+				<input type="password" class="form-control" id="userPassword" name="userPassword"
+						 placeholder="enter password" ng-model="loginData.userPassword" ng-minlength="8"
+						 ng-required="true"/>
 			</div>
-			<input type="password" class="form-control" id="userPassword" name="userPassword"
-					 placeholder="enter password" ng-model="loginData.userPassword" ng-minlength="8"
-					 ng-required="true"/>
 			<div class="alert alert-danger" role="alert" ng-messages="modalLoginForm.userPassword.$error"
 				  ng-if="modalLoginForm.userPassword.$touched"
 				  ng-hide="modalLoginForm.userPassword.$valid">
 				<p ng-message="required">Please enter your password.</p>
 			</div>
 		</div>
-	</div>
-	<button type="submit" class="btn btn-info" ng-disabled="signinForm.$invalid">Log In</button>
-	<button type="reset" class="btn btn-warning" ng-click="cancel();">Cancel</button>
-</form>
+		<button type="submit" class="btn btn-info" ng-disabled="modalLoginForm.$invalid">Log In</button>
+		<button type="reset" class="btn btn-warning" ng-click="cancel();">Cancel</button>
+	</form>
+</div>
